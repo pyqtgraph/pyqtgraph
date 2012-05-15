- Fixes to ScatterPlotItem bounding rect calculation - Moved some functionality from UIGraphicsItem upstream to GraphicsItem

graphicsItems/ScatterPlotItem.py
@@ -35,11 +35,12 @@ for k, c in coords.items():
 
 def makeSymbolPixmap(size, pen, brush, symbol):
     ## Render a spot with the given parameters to a pixmap
-    image = QtGui.QImage(size+2, size+2, QtGui.QImage.Format_ARGB32_Premultiplied)
+    penPxWidth = np.ceil(pen.width())
+    image = QtGui.QImage(size+penPxWidth, size+penPxWidth, QtGui.QImage.Format_ARGB32_Premultiplied)
     image.fill(0)
     p = QtGui.QPainter(image)
     p.setRenderHint(p.Antialiasing)
-    p.translate(size*0.5+1, size*0.5+1)
+    p.translate(image.width()*0.5, image.height()*0.5)
     p.scale(size, size)
     p.setPen(pen)
     p.setBrush(brush)
@@ -79,19 +80,16 @@ class ScatterPlotItem(GraphicsObject):
         GraphicsObject.__init__(self)
         self.setFlag(self.ItemHasNoContents, True)
         self.data = np.empty(0, dtype=[('x', float), ('y', float), ('size', float), ('symbol', 'S1'), ('pen', object), ('brush', object), ('item', object), ('data', object)])
-        #self.spots = []
-        #self.fragments = None
-        self.bounds = [None, None]
-        self.opts = {'pxMode': True}
-        #self.spotsValid = False
-        #self.itemsValid = False
+        self.bounds = [None, None]  ## caches data bounds
+        self._maxSpotWidth = 0      ## maximum size of the scale-variant portion of all spots
+        self._maxSpotPxWidth = 0    ## maximum size of the scale-invariant portion of all spots
         self._spotPixmap = None
+        self.opts = {'pxMode': True}
         
         self.setPen(200,200,200, update=False)
         self.setBrush(100,100,150, update=False)
         self.setSymbol('o', update=False)
         self.setSize(7, update=False)
-        #self.setIdentical(False, update=False)
         prof.mark('1')
         self.setData(*args, **kargs)
         prof.mark('setData')
@@ -228,6 +226,7 @@ class ScatterPlotItem(GraphicsObject):
             self.setPointData(kargs['data'], dataSet=newData)
         
         #self.updateSpots()
+        self.bounds = [None, None]
         self.generateSpotItems()
         self.sigPlotChanged.emit(self)
         
@@ -345,9 +344,30 @@ class ScatterPlotItem(GraphicsObject):
     def updateSpots(self, dataSet=None):
         if dataSet is None:
             dataSet = self.data
+        self._maxSpotWidth = 0
+        self._maxSpotPxWidth = 0
         for spot in dataSet['item']:
             spot.updateItem()
-        
+        self.measureSpotSizes(dataSet)
+
+    def measureSpotSizes(self, dataSet):
+        for spot in dataSet['item']:
+            ## keep track of the maximum spot size and pixel size
+            width = 0
+            pxWidth = 0
+            if self.opts['pxMode']:
+                pxWidth += spot.size()
+            else:
+                width += spot.size()
+            pen = spot.pen()
+            if pen.isCosmetic():
+                pxWidth += pen.width() * 2
+            else:
+                width += pen.width() * 2
+            self._maxSpotWidth = max(self._maxSpotWidth, width)
+            self._maxSpotPxWidth = max(self._maxSpotPxWidth, pxWidth)
+    
+    
     def clear(self):
         """Remove all spots from the scatter plot"""
         self.clearItems()
@@ -384,14 +404,16 @@ class ScatterPlotItem(GraphicsObject):
             d2 = d2[mask]
             
         if frac >= 1.0:
+            ## increase size of bounds based on spot size and pen width
+            px = self.pixelLength(Point(1, 0) if ax == 0 else Point(0, 1))  ## determine length of pixel along this axis
+            if px is None:
+                px = 0
             minIndex = np.argmin(d)
             maxIndex = np.argmax(d)
             minVal = d[minIndex]
             maxVal = d[maxIndex]
-            if not self.opts['pxMode']:
-                minVal -= self.data[minIndex]['size']
-                maxVal += self.data[maxIndex]['size']
-            self.bounds[ax] = (minVal, maxVal)
+            spotSize = 0.5 * (self._maxSpotWidth + px * self._maxSpotPxWidth)
+            self.bounds[ax] = (minVal-spotSize, maxVal+spotSize)
             return self.bounds[ax]
         elif frac <= 0.0:
             raise Exception("Value for parameter 'frac' must be > 0. (got %s)" % str(frac))
@@ -412,6 +434,7 @@ class ScatterPlotItem(GraphicsObject):
             for rec in self.data:
                 if rec['item'] is None:
                     rec['item'] = PathSpotItem(rec, self)
+        self.measureSpotSizes(self.data)
         self.sigPlotChanged.emit(self)
 
     def defaultSpotPixmap(self):
@@ -430,6 +453,17 @@ class ScatterPlotItem(GraphicsObject):
             ymn = 0
             ymx = 0
         return QtCore.QRectF(xmn, ymn, xmx-xmn, ymx-ymn)
+
+    def viewRangeChanged(self):
+        GraphicsObject.viewRangeChanged(self)
+        self.bounds = [None, None]
+        
+    def paint(self, p, *args):
+        ## NOTE: self.paint is disabled by this line in __init__:
+        ## self.setFlag(self.ItemHasNoContents, True)
+        p.setPen(fn.mkPen('r'))
+        p.drawRect(self.boundingRect())
+
         
     def points(self):
         return self.data['item']