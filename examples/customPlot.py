dde6a2ac4cce68ec42f0ccb04f0e27573fcc3696
Megan Kratz <meganbkratz@gmail.com>
2012-11-27 14:54:49 -0500

    work on DateAxisItem

--- examples/customPlot.py
+++ examples/customPlot.py
@@ -16,19 +16,34 @@ class DateAxis(pg.AxisItem):
     def tickStrings(self, values, scale, spacing):
         strns = []
         rng = max(values)-min(values)
-        if rng < 120:
-            return pg.AxisItem.tickStrings(self, values, scale, spacing)
-        elif rng >= 120 and rng < 3600*24:
+        #if rng < 120:
+        #    return pg.AxisItem.tickStrings(self, values, scale, spacing)
+        if rng < 3600*24:
             string = '%H:%M:%S'
+            label1 = '%b %d -'
+            label2 = ' %b %d, %Y'
         elif rng >= 3600*24 and rng < 3600*24*30:
             string = '%d'
-        elif rng >= 3600*24*30:
-            string = '%b %Y'
+            label1 = '%b - '
+            label2 = '%b, %Y'
+        elif rng >= 3600*24*30 and rng < 3600*24*30*24:
+            string = '%b'
+            label1 = '%Y -'
+            label2 = ' %Y'
+        elif rng >=3600*24*30*24:
+            string = '%Y'
+            label1 = ''
+            label2 = ''
         for x in values:
             try:
                 strns.append(time.strftime(string, time.localtime(x)))
             except ValueError:  ## Windows can't handle dates before 1970
                 strns.append('')
+        try:
+            label = time.strftime(label1, time.localtime(min(values)))+time.strftime(label2, time.localtime(max(values)))
+        except ValueError:
+            label = ''
+        #self.setLabel(text=label)
         return strns
 
 class CustomViewBox(pg.ViewBox):
