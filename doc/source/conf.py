6931eacffd2809abc74d30e4fae59151ce5088f6
Luke Campagnola <luke.campagnola@gmail.com>
2012-12-26 21:02:36 -0500

    Fixed doc build to work with new package structure.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,8 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(path, '..', '..', '..'))
-print sys.path
+sys.path.insert(0, os.path.join(path, '..', '..'))
 
 # -- General configuration -----------------------------------------------------
 
