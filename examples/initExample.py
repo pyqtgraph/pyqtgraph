30d33956fb38c0bc0fb4033c8c9e431a93b1da79
Luke Campagnola <>
2012-12-29 21:51:29 -0500

    examples fix -- prevent adding invalid entry to sys.path while searching for pyqtgraph added script for generating debian changelog from bzr log

--- examples/initExample.py
+++ examples/initExample.py
@@ -1,11 +1,21 @@
 ## make this version of pyqtgraph importable before any others
+## we do this to make sure that, when running examples, the correct library
+## version is imported (if there are multiple versions present).
 import sys, os
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-path.rstrip(os.path.sep)
-if 'pyqtgraph' in os.listdir(path):
-    sys.path.insert(0, path) ## examples adjacent to pyqtgraph (as in source)
-elif path.endswith('pyqtgraph'):
-    sys.path.insert(0, os.path.abspath(os.path.join(path, '..')))  ## examples installed inside pyqtgraph package
+
+if not hasattr(sys, 'frozen'):
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    path.rstrip(os.path.sep)
+    if 'pyqtgraph' in os.listdir(path):
+        sys.path.insert(0, path) ## examples adjacent to pyqtgraph (as in source tree)
+    else:
+        for p in sys.path:
+            if len(p) < 3:
+                continue
+            if path.startswith(p):  ## If the example is already in an importable location, promote that location
+                sys.path.remove(p)
+                sys.path.insert(0, p)
+
 
 ## should force example to use PySide instead of PyQt
 if 'pyside' in sys.argv:  
