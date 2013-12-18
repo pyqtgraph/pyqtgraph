4e9e75817fb0d2b5f6880278857621af30d7d0f1
Luke Campagnola <luke.campagnola@gmail.com>
2013-12-17 21:23:37 -0500

    Added Qt.loadUiType function for PySide Added example of simple Designer usage.

--- pyqtgraph/Qt.py
+++ pyqtgraph/Qt.py
@@ -1,4 +1,14 @@
-## Do all Qt imports from here to allow easier PyQt / PySide compatibility
+"""
+This module exists to smooth out some of the differences between PySide and PyQt4:
+
+* Automatically import either PyQt4 or PySide depending on availability
+* Allow to import QtCore/QtGui pyqtgraph.Qt without specifying which Qt wrapper
+  you want to use.
+* Declare QtCore.Signal, .Slot in PyQt4  
+* Declare loadUiType function for Pyside
+
+"""
+
 import sys, re
 
 ## Automatically determine whether to use PyQt or PySide. 
@@ -23,8 +33,41 @@ if USE_PYSIDE:
     from PySide import QtGui, QtCore, QtOpenGL, QtSvg
     import PySide
     VERSION_INFO = 'PySide ' + PySide.__version__
+    
+    # Make a loadUiType function like PyQt has
+    
+    # Credit: 
+    # http://stackoverflow.com/questions/4442286/python-code-genration-with-pyside-uic/14195313#14195313
+
+    def loadUiType(uiFile):
+        """
+        Pyside "loadUiType" command like PyQt4 has one, so we have to convert the ui file to py code in-memory first    and then execute it in a special frame to retrieve the form_class.
+        """
+        import pysideuic
+        import xml.etree.ElementTree as xml
+        from io import StringIO
+        
+        parsed = xml.parse(uiFile)
+        widget_class = parsed.find('widget').get('class')
+        form_class = parsed.find('class').text
+
+        with open(uiFile, 'r') as f:
+            o = StringIO()
+            frame = {}
+
+            pysideuic.compileUi(f, o, indent=0)
+            pyc = compile(o.getvalue(), '<string>', 'exec')
+            exec(pyc, frame)
+
+            #Fetch the base_class and form class based on their type in the xml from designer
+            form_class = frame['Ui_%s'%form_class]
+            base_class = eval('QtGui.%s'%widget_class)
+
+        return form_class, base_class
+    
+    
 else:
-    from PyQt4 import QtGui, QtCore
+    from PyQt4 import QtGui, QtCore, uic
     try:
         from PyQt4 import QtSvg
     except ImportError:
@@ -33,6 +76,9 @@ else:
         from PyQt4 import QtOpenGL
     except ImportError:
         pass
+
+
+    loadUiType = uic.loadUiType
 
     QtCore.Signal = QtCore.pyqtSignal
     VERSION_INFO = 'PyQt4 ' + QtCore.PYQT_VERSION_STR + ' Qt ' + QtCore.QT_VERSION_STR
@@ -43,6 +89,6 @@ versionReq = [4, 7]
 QtVersion = PySide.QtCore.__version__ if USE_PYSIDE else QtCore.QT_VERSION_STR
 m = re.match(r'(\d+)\.(\d+).*', QtVersion)
 if m is not None and list(map(int, m.groups())) < versionReq:
-    print(map(int, m.groups()))
+    print(list(map(int, m.groups())))
     raise Exception('pyqtgraph requires Qt version >= %d.%d  (your version is %s)' % (versionReq[0], versionReq[1], QtVersion))
 
