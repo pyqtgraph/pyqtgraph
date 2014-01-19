eae32af0c7113a85262eb86a25bb554231a15807
Luke Campagnola <luke.campagnola@gmail.com>
2014-01-18 23:30:03 -0500

    Added symbol to Legend example

--- examples/Legend.py
+++ examples/Legend.py
@@ -14,7 +14,7 @@ plt.addLegend()
 #l = pg.LegendItem((100,60), offset=(70,30))  # args are (size, offset)
 #l.setParentItem(plt.graphicsItem())   # Note we do NOT call plt.addItem in this case
 
-c1 = plt.plot([1,3,2,4], pen='r', name='red plot')
+c1 = plt.plot([1,3,2,4], pen='r', symbol='o', symbolPen='r', symbolBrush=0.5, name='red plot')
 c2 = plt.plot([2,1,4,3], pen='g', fillLevel=0, fillBrush=(255,255,255,30), name='green plot')
 #l.addItem(c1, 'red plot')
 #l.addItem(c2, 'green plot')
