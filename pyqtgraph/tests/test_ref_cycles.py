0479507dbbba308e9b416e917cf5aca9310a3164
Luke Campagnola <luke.campagnola@gmail.com>
2014-04-27 13:07:31 -0400

    Expanded ref checks Fixed ref cycle in ImageItem -> HistogramLutItem

--- pyqtgraph/tests/test_ref_cycles.py
+++ pyqtgraph/tests/test_ref_cycles.py
@@ -7,54 +7,42 @@ import numpy as np
 import gc, weakref
 app = pg.mkQApp()
 
-def processEvents():
-    for i in range(3):
-        gc.collect()
-        app.processEvents()
-        # processEvents ignored DeferredDelete events; we must process these
-        # manually.
-        app.sendPostedEvents(None, pg.QtCore.QEvent.DeferredDelete)
+def assert_alldead(refs):
+    for ref in refs:
+        assert ref() is None
 
-#def test_PlotItem():
-    #for i in range(10):
-        #plt = pg.PlotItem()
-        #plt.plot(np.random.normal(size=10000))
-    #processEvents()
-    
-    #ot = pg.debug.ObjTracker()
-    
-    #plots = []
-    #for i in range(10):
-        #plt = pg.PlotItem()
-        #plt.plot(np.random.normal(size=10000))
-        #plots.append(plt)
-    #processEvents()
-
-    #ot.diff()
-    
-    #del plots
-    #processEvents()
-    
-    #ot.diff()
-    
-    
-    #return ot
+def mkrefs(*objs):
+    return map(weakref.ref, objs)
 
 def test_PlotWidget():
-    def mkref(*args, **kwds):
-        iv = pg.PlotWidget(*args, **kwds)
-        return weakref.ref(iv)
+    def mkobjs(*args, **kwds):
+        w = pg.PlotWidget(*args, **kwds)
+        data = pg.np.array([1,5,2,4,3])
+        c = w.plot(data, name='stuff')
+        w.addLegend()
+        
+        # test that connections do not keep objects alive
+        w.plotItem.vb.sigRangeChanged.connect(mkrefs)
+        app.focusChanged.connect(w.plotItem.vb.invertY)
+        
+        # return weakrefs to a bunch of objects that should die when the scope exits.
+        return mkrefs(w, c, data, w.plotItem, w.plotItem.vb, w.plotItem.getMenu(), w.plotItem.getAxis('left'))
     
     for i in range(5):
-        assert mkref()() is None
+        assert_alldead(mkobjs())
     
 def test_ImageView():
-    def mkref(*args, **kwds):
-        iv = pg.ImageView(*args, **kwds)
-        return weakref.ref(iv)
+    def mkobjs():
+        iv = pg.ImageView()
+        data = np.zeros((10,10,5))
+        iv.setImage(data)
+        
+        return mkrefs(iv, iv.imageItem, iv.view, iv.ui.histogram, data)
     
     for i in range(5):
-        assert mkref()() is None
+        assert_alldead(mkobjs())
+
+
     
 if __name__ == '__main__':
     ot = test_PlotItem()
