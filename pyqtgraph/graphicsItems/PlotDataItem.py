392d2a3475b4247c277d2c3b051d044e37870adc
mrussell <matthew.russell@arkex.com>
2015-07-06 16:21:06 +0100

    Log scale and fft transform fix
    If the plotted data is fourier transformed and an x log scale is chosen, the first bin causes an error because np.log10(0) doesn't make any sense.

--- pyqtgraph/graphicsItems/PlotDataItem.py
+++ pyqtgraph/graphicsItems/PlotDataItem.py
@@ -523,6 +523,10 @@ class PlotDataItem(GraphicsObject):
                 #y = y[::ds]
             if self.opts['fftMode']:
                 x,y = self._fourierTransform(x, y)
+                # Ignore the first bin for fft data if we have a logx scale
+                if self.opts['logMode'][0]:
+                    x=x[1:]
+                    y=y[1:]                
             if self.opts['logMode'][0]:
                 x = np.log10(x)
             if self.opts['logMode'][1]:
