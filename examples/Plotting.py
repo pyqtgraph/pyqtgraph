b9b0874ae72d26c5e6fd5772ea1c2ef3676c2a06
Alessandro Bacchini <alessandro.bacchini@henesis.eu>
2016-03-03 08:31:39 +0100

    Check for ViewBox child items

--- examples/Plotting.py
+++ examples/Plotting.py
@@ -119,6 +119,8 @@ def onRangeChanged(arg1, arg2):
 
 #p7.sigRangeChanged.connect(onRangeChanged)
 
+print p7.vb.childItems()
+
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
