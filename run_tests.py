3631cfdb7a958b3a2827c8a9fa88cf5ae3918192
Eric Dill <edill@bnl.gov>
2016-03-23 12:14:51 -0400

    TST/CI: Add test script. Update .travis.yml
    On the Python Test Podcast, @nedbat recommended using the coverage
    package as the execuable with the `run` subcommand, e.g., `coverage run
    run_tests.py`. This guarantees that all code executed goes through the
    `coverage` pipeline and code reporting stats are accurate.  This PR
    makes those changes by adding a test script that provides the same
    results (or better) as running `py.test -vrxs --cov pyqtgraph`.
    Additionally, @nedbat recommends using coverage directly instead of the
    coverage plugins for the various testing frameworks (nose coverage,
    pytest-cov, etc...) because those plugins can lag behind development of
    coverage.

---
+++ run_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import pytest
+
+if __name__ == '__main__':
+    # show output results from every test function
+    args = ['-v']
+    # show the message output for skipped and expected failure tests
+    args.append('-rxs')
+    if len(sys.argv) > 1:
+        args.extend(sys.argv[1:])
+    print('pytest arguments: {}'.format(args))
+    # call pytest and exit with the return code from pytest so that
+    # travis will fail correctly if tests fail
+    sys.exit(pytest.main(args))
