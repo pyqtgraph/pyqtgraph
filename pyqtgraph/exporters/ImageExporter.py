e975cbd3ae702eb94dd722216bd6da13b976c559
Karl Bedrich <karl@bedrich.de>
2016-11-10 15:44:57 +0000

    moved some functionality from method 'export' to new method 'getSupportedFormats' making it acessible from outside

--- pyqtgraph/exporters/ImageExporter.py
+++ pyqtgraph/exporters/ImageExporter.py
@@ -43,18 +43,23 @@ class ImageExporter(Exporter):
         
     def parameters(self):
         return self.params
-    
+
+    @staticmethod
+    def getSupportedImageFormats():
+        if USE_PYSIDE:
+            filter = ["*."+str(f) for f in QtGui.QImageWriter.supportedImageFormats()]
+        else:
+            filter = ["*."+bytes(f).decode('utf-8') for f in QtGui.QImageWriter.supportedImageFormats()]
+        preferred = ['*.png', '*.tif', '*.jpg']
+        for p in preferred[::-1]:
+            if p in filter:
+                filter.remove(p)
+                filter.insert(0, p) 
+        return filter  
+
     def export(self, fileName=None, toBytes=False, copy=False):
         if fileName is None and not toBytes and not copy:
-            if USE_PYSIDE:
-                filter = ["*."+str(f) for f in QtGui.QImageWriter.supportedImageFormats()]
-            else:
-                filter = ["*."+bytes(f).decode('utf-8') for f in QtGui.QImageWriter.supportedImageFormats()]
-            preferred = ['*.png', '*.tif', '*.jpg']
-            for p in preferred[::-1]:
-                if p in filter:
-                    filter.remove(p)
-                    filter.insert(0, p)
+            filter = self.getSupportedImageFormats()
             self.fileSaveDialog(filter=filter)
             return
             
