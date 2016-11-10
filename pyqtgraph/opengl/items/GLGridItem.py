8544435e324ed89975f608005027600803cbd6ab
Karl Bedrich <karl@bedrich.de>
2016-11-10 17:49:56 +0000

    NEW method setColor

--- pyqtgraph/opengl/items/GLGridItem.py
+++ pyqtgraph/opengl/items/GLGridItem.py
@@ -17,11 +17,22 @@ class GLGridItem(GLGraphicsItem):
         GLGraphicsItem.__init__(self)
         self.setGLOptions(glOptions)
         self.antialias = antialias
+        self.setColor(color)   
         if size is None:
             size = QtGui.QVector3D(20,20,1)
         self.setSize(size=size)
         self.setSpacing(1, 1, 1)
-    
+
+    def setColor(self, color):
+        """
+        set the color of the Grid
+        'color' can be any value accepted by QColor
+        """
+        if color == None:
+            self.color = (1, 1, 1, .3)
+        else:
+            self.color = QtGui.QColor(color).getRgbF()
+
     def setSize(self, x=None, y=None, z=None, size=None):
         """
         Set the size of the axes (in its local coordinate system; this does not affect the transform)
@@ -67,7 +78,7 @@ class GLGridItem(GLGraphicsItem):
         xs,ys,zs = self.spacing()
         xvals = np.arange(-x/2., x/2. + xs*0.001, xs) 
         yvals = np.arange(-y/2., y/2. + ys*0.001, ys) 
-        glColor4f(1, 1, 1, .3)
+        glColor4f(*self.color)
         for x in xvals:
             glVertex3f(x, yvals[0], 0)
             glVertex3f(x,  yvals[-1], 0)
