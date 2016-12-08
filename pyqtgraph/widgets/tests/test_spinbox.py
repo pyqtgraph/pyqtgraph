cd7683b61db5171d71487832ab5886509a945ac5
Luke Campagnola <luke.campagnola@gmail.com>
2016-12-08 10:14:18 -0800

    Fix unit tests

--- pyqtgraph/widgets/tests/test_spinbox.py
+++ pyqtgraph/widgets/tests/test_spinbox.py
@@ -4,7 +4,7 @@ pg.mkQApp()
 
 def test_spinbox():
     sb = pg.SpinBox()
-    assert sb.opts['decimals'] == 3
+    assert sb.opts['decimals'] == 6
     assert sb.opts['int'] is False
     
     # table  of test conditions:
@@ -16,7 +16,7 @@ def test_spinbox():
         (1000, '1e+03', dict(decimals=2)),
         (1000000, '1e+06', dict(int=True, decimals=6)),
         (12345678955, '12345678955', dict(int=True, decimals=100)),
-        (1.45e-9, '1.45e-9 A', dict(int=False, decimals=6, suffix='A', siPrefix=False)),
+        (1.45e-9, '1.45e-09 A', dict(int=False, decimals=6, suffix='A', siPrefix=False)),
         (1.45e-9, '1.45 nA', dict(int=False, decimals=6, suffix='A', siPrefix=True)),
     ]
     
