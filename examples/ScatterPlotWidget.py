9a7e1d206350cb42423bc4d2f05c404dec32df84
zvodd <zv.odd.101@gmail.com>
2017-02-20 01:39:16 +1100

    ScatterPlotWidget deprecated boolean index now int
    Numpy doesn't like boolean indexes for ndarrays
    ```
    IndexError: in the future, 0-d boolean arrays will be interpreted as a valid boolean index
    ```

--- examples/ScatterPlotWidget.py
+++ examples/ScatterPlotWidget.py
@@ -33,19 +33,21 @@ strings = ['Type-A', 'Type-B', 'Type-C', 'Type-D', 'Type-E']
 typeInds = np.random.randint(5, size=1000)
 data['type'] = np.array(strings)[typeInds]
 data['x_pos'] = np.random.normal(size=1000)
-data['x_pos'][data['type'] == 'Type-A'] -= 1
-data['x_pos'][data['type'] == 'Type-B'] -= 1
-data['x_pos'][data['type'] == 'Type-C'] += 2
-data['x_pos'][data['type'] == 'Type-D'] += 2
-data['x_pos'][data['type'] == 'Type-E'] += 2
+
+
+data['x_pos'][int(data['type'] == 'Type-A')] -= 1
+data['x_pos'][int(data['type'] == 'Type-B')] -= 1
+data['x_pos'][int(data['type'] == 'Type-C')] += 2
+data['x_pos'][int(data['type'] == 'Type-D')] += 2
+data['x_pos'][int(data['type'] == 'Type-E')] += 2
 data['y_pos'] = np.random.normal(size=1000) + data['x_pos']*0.1
-data['y_pos'][data['type'] == 'Type-A'] += 3
-data['y_pos'][data['type'] == 'Type-B'] += 3
+data['y_pos'][int(data['type'] == 'Type-A')] += 3
+data['y_pos'][int(data['type'] == 'Type-B')] += 3
 data['amplitude'] = data['x_pos'] * 1.4 + data['y_pos'] + np.random.normal(size=1000, scale=0.4)
 data['count'] = (np.random.exponential(size=1000, scale=100) * data['x_pos']).astype(int)
 data['decay'] = np.random.normal(size=1000, scale=1e-3) + data['amplitude'] * 1e-4
-data['decay'][data['type'] == 'Type-A'] /= 2
-data['decay'][data['type'] == 'Type-E'] *= 3
+data['decay'][int(data['type'] == 'Type-A')] /= 2
+data['decay'][int(data['type'] == 'Type-E')] *= 3
 
 
 # Create ScatterPlotWidget and configure its fields
