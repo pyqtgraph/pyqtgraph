72a715753853267f3513cae799ccca831f4f9b29
Alexander Manakov <kokokostation@gmail.com>
2017-08-04 13:56:37 +0300

    Added essential if.

--- pyqtgraph/graphicsItems/ScatterPlotItem.py
+++ pyqtgraph/graphicsItems/ScatterPlotItem.py
@@ -648,6 +648,9 @@ class ScatterPlotItem(GraphicsObject):
             d = d[mask]
             d2 = d2[mask]
 
+            if d.size == 0:
+                return (None, None)
+
         if frac >= 1.0:
             self.bounds[ax] = (np.nanmin(d) - self._maxSpotWidth*0.7072, np.nanmax(d) + self._maxSpotWidth*0.7072)
             return self.bounds[ax]
