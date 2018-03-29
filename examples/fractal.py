358b1539eee6d45f75169bf4471604d540c8e8d5
Luke Campagnola <luke.campagnola@gmail.com>
2018-03-29 08:49:42 -0700

    add reduce import to fractal demo

--- examples/fractal.py
+++ examples/fractal.py
@@ -4,6 +4,7 @@ Displays an interactive Koch fractal
 """
 import initExample ## Add path to library (just for examples; you do not need this)
 
+from functools import reduce
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
@@ -111,12 +112,4 @@ if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
-
-
-
-
-
-
-
-
     
