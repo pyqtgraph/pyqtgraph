16616c77b7aa637712c949245d9d4945d88592a0
Tran Duy Hoa <hoatranduy@gmail.com>
2018-10-26 23:06:23 +0200

    Fix bug in GLViewWidget.py
    call debug.printExc() instead of pyqtgraph.debug.printExc()

--- pyqtgraph/opengl/GLViewWidget.py
+++ pyqtgraph/opengl/GLViewWidget.py
@@ -427,7 +427,7 @@ class GLViewWidget(QtOpenGL.QGLWidget):
         ver = glGetString(GL_VERSION).split()[0]
         if int(ver.split('.')[0]) < 2:
             from .. import debug
-            pyqtgraph.debug.printExc()
+            debug.printExc()
             raise Exception(msg + " The original exception is printed above; however, pyqtgraph requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
         else:
             raise
