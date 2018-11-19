5d55a35c268b01f68c4aa7f64a9534b702abee07
Paul Debus <paul.debus@uni-weimar.de>
2018-11-19 14:36:00 +0100

    fix encoding error in checkOpenGLVersion

--- pyqtgraph/opengl/GLViewWidget.py
+++ pyqtgraph/opengl/GLViewWidget.py
@@ -425,7 +425,7 @@ class GLViewWidget(QtOpenGL.QGLWidget):
     def checkOpenGLVersion(self, msg):
         ## Only to be called from within exception handler.
         ver = glGetString(GL_VERSION).split()[0]
-        if int(ver.split('.')[0]) < 2:
+        if int(ver.split(b'.')[0]) < 2:
             from .. import debug
             pyqtgraph.debug.printExc()
             raise Exception(msg + " The original exception is printed above; however, pyqtgraph requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
