02b7532706ea36a811ad64a85063e6469fcacc11
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-12-20 18:40:36 +0100

    Remove use of GraphicsScene._addressCache in translateGraphicsItem
    Use QGraphicsItem.toQGrapicsObject on the item instead.
    
    This probably is not even needed since PyQt 4.9

--- pyqtgraph/graphicsItems/GraphicsItem.py
+++ pyqtgraph/graphicsItems/GraphicsItem.py
@@ -19,8 +19,8 @@ class GraphicsItem(object):
     The GraphicsView system places a lot of emphasis on the notion that the graphics within the scene should be device independent--you should be able to take the same graphics and display them on screens of different resolutions, printers, export to SVG, etc. This is nice in principle, but causes me a lot of headache in practice. It means that I have to circumvent all the device-independent expectations any time I want to operate in pixel coordinates rather than arbitrary scene coordinates. A lot of the code in GraphicsItem is devoted to this task--keeping track of view widgets and device transforms, computing the size and shape of a pixel in local item coordinates, etc. Note that in item coordinates, a pixel does not have to be square or even rectangular, so just asking how to increase a bounding rect by 2px can be a rather complex task.
     """
     _pixelVectorGlobalCache = LRUCache(100, 70)
-    
-    def __init__(self, register=True):
+
+    def __init__(self, register=None):
         if not hasattr(self, '_qtBaseClass'):
             for b in self.__class__.__bases__:
                 if issubclass(b, QtGui.QGraphicsItem):
@@ -28,15 +28,18 @@ class GraphicsItem(object):
                     break
         if not hasattr(self, '_qtBaseClass'):
             raise Exception('Could not determine Qt base class for GraphicsItem: %s' % str(self))
-        
+
         self._pixelVectorCache = [None, None]
         self._viewWidget = None
         self._viewBox = None
         self._connectedView = None
         self._exportOpts = False   ## If False, not currently exporting. Otherwise, contains dict of export options.
-        if register:
-            GraphicsScene.registerObject(self)  ## workaround for pyqt bug in graphicsscene.items()
-                    
+        if register is not None and register:
+            warnings.warn(
+                "'register' argument is deprecated and does nothing",
+                DeprecationWarning, stacklevel=2
+            )
+
     def getViewWidget(self):
         """
         Return the view widget for this item. 
