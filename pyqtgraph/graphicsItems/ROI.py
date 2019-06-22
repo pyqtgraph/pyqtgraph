b958d1b42456ba0c53a3741ebe04098bb1dc87d8
2xB <2xB@users.noreply.github.com>
2019-06-22 04:08:38 +0200

    Fix deprecation warning of multi-dimensional tuples

--- pyqtgraph/graphicsItems/ROI.py
+++ pyqtgraph/graphicsItems/ROI.py
@@ -1667,7 +1667,7 @@ class MultiRectROI(QtGui.QGraphicsObject):
         ms = min([r.shape[axes[1]] for r in rgns])
         sl = [slice(None)] * rgns[0].ndim
         sl[axes[1]] = slice(0,ms)
-        rgns = [r[sl] for r in rgns]
+        rgns = [r[tuple(sl)] for r in rgns]
         #print [r.shape for r in rgns], axes
         
         return np.concatenate(rgns, axis=axes[0])
