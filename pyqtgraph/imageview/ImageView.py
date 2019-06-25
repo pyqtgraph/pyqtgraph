55d9e8888b77c50a1ba568c9dd3153f5f288976f
Kenneth Lyons <ixjlyons@gmail.com>
2019-06-25 16:08:18 -0700

    Allow last image in stack to be selected by slider in ImageView

--- pyqtgraph/imageview/ImageView.py
+++ pyqtgraph/imageview/ImageView.py
@@ -740,7 +740,7 @@ class ImageView(QtGui.QWidget):
             return (0,0)
         
         t = slider.value()
-        
+
         xv = self.tVals
         if xv is None:
             ind = int(t)
@@ -748,7 +748,7 @@ class ImageView(QtGui.QWidget):
             if len(xv) < 2:
                 return (0,0)
             totTime = xv[-1] + (xv[-1]-xv[-2])
-            inds = np.argwhere(xv < t)
+            inds = np.argwhere(xv <= t)
             if len(inds) < 1:
                 return (0,t)
             ind = inds[-1,0]
