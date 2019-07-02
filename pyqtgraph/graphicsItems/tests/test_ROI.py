7641d2635b030587db30e583c6ab48aa8b162918
Luke Campagnola <ausqualius@yahoo.com>
2019-07-01 20:30:16 -0700

    A little more specific about how to relax test..

--- pyqtgraph/graphicsItems/tests/test_ROI.py
+++ pyqtgraph/graphicsItems/tests/test_ROI.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pytest
 import pyqtgraph as pg
@@ -134,7 +135,11 @@ def check_getArrayRegion(roi, name, testResize=True, transpose=False):
     img2.setImage(rgn[0, ..., 0])
     app.processEvents()
     # on windows, one edge of one ROI handle is shifted slightly; letting this slide with pxCount=10
-    assertImageApproved(win, name+'/roi_getarrayregion_inverty', 'Simple ROI region selection, view inverted.', pxCount=10)
+    if sys.platform == 'win32' and pg.Qt.QT_LIB in ('PyQt4', 'PySide'):
+        pxCount = 10
+    else:
+        pxCount=-1
+    assertImageApproved(win, name+'/roi_getarrayregion_inverty', 'Simple ROI region selection, view inverted.', pxCount=pxCount)
 
     roi.setState(initState)
     img1.resetTransform()
