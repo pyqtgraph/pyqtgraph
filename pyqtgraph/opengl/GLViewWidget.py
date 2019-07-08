d72795b30f675968af86a7d32f1d73f08da0ce58
2xB <31772910+2xB@users.noreply.github.com>
2019-07-08 17:14:27 +0200

    Add missing bracket

--- pyqtgraph/opengl/GLViewWidget.py
+++ pyqtgraph/opengl/GLViewWidget.py
@@ -185,7 +185,7 @@ class GLViewWidget(QtOpenGL.QGLWidget):
         if viewport is None:
             viewport = self.getViewport()
             
-        raise RuntimeError("paintGL called. region={}, viewport={}, useItemNames={}".format(repr(region), repr(viewport), repr(useItemNames))
+        raise RuntimeError("paintGL called. region={}, viewport={}, useItemNames={}".format(repr(region), repr(viewport), repr(useItemNames)))
             
         glViewport(*viewport)
             
