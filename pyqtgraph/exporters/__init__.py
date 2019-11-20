98980c8d2da39f50191aefbf1714179a541469ea
aplans <aplans@ingeniamc.com>
2019-11-20 15:09:36 +0100

    MOT3-1136 Removed matplotlib option from exporter

--- pyqtgraph/exporters/__init__.py
+++ pyqtgraph/exporters/__init__.py
@@ -1,7 +1,7 @@
 from .Exporter import Exporter
 from .ImageExporter import *
 from .SVGExporter import *
-from .Matplotlib import *
+# from .Matplotlib import *
 from .CSVExporter import *
 from .PrintExporter import *
 from .HDF5Exporter import *
