1b80893a01e6fa4c6a3481a3ca373c5c1f076f76
2xB <2xB@users.noreply.github.com>
2020-04-27 21:08:30 +0200

    DateAxisItem: Fix documentation to zoomLevels
    zoomLevels is not intended to be set by the user (see discussion in converstation from
    https://github.com/pyqtgraph/pyqtgraph/pull/1154/files#diff-aefdb23660d0963df0dff3a116baded8
    ). Also, `zoomLevelWidths` does currently not exist.
    This commit adapts the documentation to reflect that.

--- pyqtgraph/graphicsItems/DateAxisItem.py
+++ pyqtgraph/graphicsItems/DateAxisItem.py
@@ -260,9 +260,8 @@ class DateAxisItem(AxisItem):
         overriding this function or setting a different set of zoom levels
         than the default one. The `zoomLevels` variable is a dictionary with the
         maximal distance of ticks in seconds which are allowed for each zoom level
-        before the axis switches to the next coarser level. To create custom
-        zoom levels, override this function and provide custom `zoomLevelWidths` and
-        `zoomLevels`.
+        before the axis switches to the next coarser level. To customize the zoom level
+        selection, override this function.
         """
         padding = 10
         
