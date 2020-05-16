ae8fc195da919adcc7466746d8daf1040deee849
Kenneth Lyons <ixjlyons@gmail.com>
2020-05-16 10:14:52 -0700

    Disable inherited docstrings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -88,6 +88,7 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+autodoc_inherit_docstrings = False
 
 # -- Options for HTML output ---------------------------------------------------
 
