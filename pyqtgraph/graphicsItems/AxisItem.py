e08ac110f594a7cbe55f38608542237e0a855ef5
Paul Müller <paulmueller@users.noreply.github.com>
2020-05-30 22:53:38 +0200

    pretty-print log-scale axes labels (#1097)
    * pretty-print log-scale axes labels
    
    * only pretty-print in python 3

--- pyqtgraph/graphicsItems/AxisItem.py
+++ pyqtgraph/graphicsItems/AxisItem.py
@@ -4,6 +4,7 @@ from ..python2_3 import asUnicode
 import numpy as np
 from ..Point import Point
 from .. import debug as debug
+import sys
 import weakref
 from .. import functions as fn
 from .. import getConfigOption
@@ -813,7 +814,37 @@ class AxisItem(GraphicsWidget):
         return strings
 
     def logTickStrings(self, values, scale, spacing):
-        return ["%0.1g"%x for x in 10 ** np.array(values).astype(float) * np.array(scale)]
+        estrings = ["%0.1g"%x for x in 10 ** np.array(values).astype(float) * np.array(scale)]
+
+        if sys.version_info < (3, 0):
+            # python 2 does not support unicode strings like that
+            return estrings
+        else:  # python 3+
+            convdict = {"0": "⁰",
+                        "1": "¹",
+                        "2": "²",
+                        "3": "³",
+                        "4": "⁴",
+                        "5": "⁵",
+                        "6": "⁶",
+                        "7": "⁷",
+                        "8": "⁸",
+                        "9": "⁹",
+                        }
+            dstrings = []
+            for e in estrings:
+                if e.count("e"):
+                    v, p = e.split("e")
+                    sign = "⁻" if p[0] == "-" else ""
+                    pot = "".join([convdict[pp] for pp in p[1:].lstrip("0")])
+                    if v == "1":
+                        v = ""
+                    else:
+                        v = v + "·"
+                    dstrings.append(v + "10" + sign + pot)
+                else:
+                    dstrings.append(e)
+            return dstrings
 
     def generateDrawSpecs(self, p):
         """
