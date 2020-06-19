6283e1b61c92fe9b16b9914a615bc05ccff79957
jeremysee2 <jeremysee_2@hotmail.com>
2020-06-19 10:28:11 +0800

    checkOpenGLVersion exception for OpenGL ES

--- pyqtgraph/opengl/GLViewWidget.py
+++ pyqtgraph/opengl/GLViewWidget.py
@@ -427,12 +427,15 @@ class GLViewWidget(QtOpenGL.QGLWidget):
     def checkOpenGLVersion(self, msg):
         ## Only to be called from within exception handler.
         ver = glGetString(GL_VERSION).split()[0]
-        if int(ver.split(b'.')[0]) < 2:
-            from .. import debug
-            debug.printExc()
-            raise Exception(msg + " The original exception is printed above; however, pyqtgraph requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
-        else:
-            raise
+        try:
+            if int(ver.split(b'.')[0]) < 2:
+                from .. import debug
+                debug.printExc()
+                raise Exception(msg + " The original exception is printed above; however, pyqtgraph requires OpenGL version 2.0 or greater for many of its 3D features and your OpenGL version is %s. Installing updated display drivers may resolve this issue." % ver)
+            else:
+                raise
+        except ValueError:
+            raise Exception("pyqtgraph requires OpenGL 2.0 (non-ES) for its 3D Features in GLViewWidget. Please check that you are not using OpenGL ES")
             
     def readQImage(self):
         """
