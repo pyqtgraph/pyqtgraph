390edf5111ebb9542d6059ac07211c21bb9d12fd
Kenneth Lyons <ixjlyons@gmail.com>
2020-06-27 13:56:59 -0700

    Handle axis SI prefix scaling in MatplotlibExporter

--- pyqtgraph/exporters/Matplotlib.py
+++ pyqtgraph/exporters/Matplotlib.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from ..Qt import QtGui, QtCore
 from .Exporter import Exporter
 from .. import PlotItem
@@ -31,6 +32,7 @@ publication. Fonts are not vectorized (outlined), and window colors are white.
 class MatplotlibExporter(Exporter):
     Name = "Matplotlib Window"
     windows = []
+
     def __init__(self, item):
         Exporter.__init__(self, item)
         
@@ -55,58 +57,72 @@ class MatplotlibExporter(Exporter):
                 ax.xaxis.set_ticks_position('bottom')
     
     def export(self, fileName=None):
+        if not isinstance(self.item, PlotItem):
+            raise Exception("MatplotlibExporter currently only works with PlotItem")
+    
+        mpw = MatplotlibWindow()
+        MatplotlibExporter.windows.append(mpw)
+
+        fig = mpw.getFigure()
+
+        xax = self.item.getAxis('bottom')
+        yax = self.item.getAxis('left')
         
-        if isinstance(self.item, PlotItem):
-            mpw = MatplotlibWindow()
-            MatplotlibExporter.windows.append(mpw)
+        # get labels from the graphic item
+        xlabel = xax.label.toPlainText()
+        ylabel = yax.label.toPlainText()
+        title = self.item.titleLabel.text
 
-            stdFont = 'Arial'
-            
-            fig = mpw.getFigure()
-            
-            # get labels from the graphic item
-            xlabel = self.item.axes['bottom']['item'].label.toPlainText()
-            ylabel = self.item.axes['left']['item'].label.toPlainText()
-            title = self.item.titleLabel.text
+        # if axes use autoSIPrefix, scale the data before exporting
+        xscale = yscale = 1.0
+        if xax.autoSIPrefix:
+            xscale = xax.autoSIPrefixScale
+        if yax.autoSIPrefix:
+            yscale = yax.autoSIPrefixScale
 
-            ax = fig.add_subplot(111, title=title)
-            ax.clear()
-            self.cleanAxes(ax)
-            #ax.grid(True)
-            for item in self.item.curves:
-                x, y = item.getData()
-                opts = item.opts
-                pen = fn.mkPen(opts['pen'])
-                if pen.style() == QtCore.Qt.NoPen:
-                    linestyle = ''
-                else:
-                    linestyle = '-'
-                color = tuple([c/255. for c in fn.colorTuple(pen.color())])
-                symbol = opts['symbol']
-                if symbol == 't':
-                    symbol = '^'
-                symbolPen = fn.mkPen(opts['symbolPen'])
-                symbolBrush = fn.mkBrush(opts['symbolBrush'])
-                markeredgecolor = tuple([c/255. for c in fn.colorTuple(symbolPen.color())])
-                markerfacecolor = tuple([c/255. for c in fn.colorTuple(symbolBrush.color())])
-                markersize = opts['symbolSize']
-                
-                if opts['fillLevel'] is not None and opts['fillBrush'] is not None:
-                    fillBrush = fn.mkBrush(opts['fillBrush'])
-                    fillcolor = tuple([c/255. for c in fn.colorTuple(fillBrush.color())])
-                    ax.fill_between(x=x, y1=y, y2=opts['fillLevel'], facecolor=fillcolor)
-                
-                pl = ax.plot(x, y, marker=symbol, color=color, linewidth=pen.width(), 
-                        linestyle=linestyle, markeredgecolor=markeredgecolor, markerfacecolor=markerfacecolor,
-                        markersize=markersize)
-                xr, yr = self.item.viewRange()
-                ax.set_xbound(*xr)
-                ax.set_ybound(*yr)
-            ax.set_xlabel(xlabel)  # place the labels.
-            ax.set_ylabel(ylabel)
-            mpw.draw()
-        else:
-            raise Exception("Matplotlib export currently only works with plot items")
+        ax = fig.add_subplot(111, title=title)
+        ax.clear()
+        self.cleanAxes(ax)
+        #ax.grid(True)
+        for item in self.item.curves:
+            x, y = item.getData()
+
+            # account for SI prefix scaling
+            x = x * xscale
+            y = y * yscale
+
+            opts = item.opts
+            pen = fn.mkPen(opts['pen'])
+            if pen.style() == QtCore.Qt.NoPen:
+                linestyle = ''
+            else:
+                linestyle = '-'
+            color = tuple([c/255. for c in fn.colorTuple(pen.color())])
+            symbol = opts['symbol']
+            if symbol == 't':
+                symbol = '^'
+            symbolPen = fn.mkPen(opts['symbolPen'])
+            symbolBrush = fn.mkBrush(opts['symbolBrush'])
+            markeredgecolor = tuple([c/255. for c in fn.colorTuple(symbolPen.color())])
+            markerfacecolor = tuple([c/255. for c in fn.colorTuple(symbolBrush.color())])
+            markersize = opts['symbolSize']
+            
+            if opts['fillLevel'] is not None and opts['fillBrush'] is not None:
+                fillBrush = fn.mkBrush(opts['fillBrush'])
+                fillcolor = tuple([c/255. for c in fn.colorTuple(fillBrush.color())])
+                ax.fill_between(x=x, y1=y, y2=opts['fillLevel'], facecolor=fillcolor)
+            
+            pl = ax.plot(x, y, marker=symbol, color=color, linewidth=pen.width(), 
+                    linestyle=linestyle, markeredgecolor=markeredgecolor, markerfacecolor=markerfacecolor,
+                    markersize=markersize)
+
+            xr, yr = self.item.viewRange()
+            ax.set_xbound(xr[0]*xscale, xr[1]*xscale)
+            ax.set_ybound(yr[0]*yscale, yr[1]*yscale)
+
+        ax.set_xlabel(xlabel)  # place the labels.
+        ax.set_ylabel(ylabel)
+        mpw.draw()
                 
 MatplotlibExporter.register()        
         
