603e1b98bc9e115a6cc66492c15b1b502ae3e480
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2020-07-16 21:13:33 +0200

    Fix line width not being set

--- pyqtgraph/graphicsItems/PlotCurveItem.py
+++ pyqtgraph/graphicsItems/PlotCurveItem.py
@@ -580,6 +580,7 @@ class PlotCurveItem(GraphicsObject):
                 if pen.isCosmetic() and width < 1:
                     width = 1
                 gl.glPointSize(width)
+                gl.glLineWidth(width)
                 gl.glEnable(gl.GL_LINE_SMOOTH)
                 gl.glEnable(gl.GL_BLEND)
                 gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
