a7aa2272508527a612c647852185839366c88db3
angulartist <belabesmohammed@gmail.com>
2020-07-19 15:48:25 +0200

    Enhancement: [Issue/812]: Update examples/MultiPlotWidget.py with usage of pen kwarg

--- examples/MultiPlotWidget.py
+++ examples/MultiPlotWidget.py
@@ -30,7 +30,7 @@ ma = MetaArray(data, info=[
         ]}, 
     {'name': 'Time', 'values': linspace(0., 1., 1000), 'units': 's'}
     ])
-pw.plot(ma)
+pw.plot(ma, pen=pg.mkPen('b', width=3))
 
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
