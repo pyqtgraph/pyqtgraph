7bf4e597d91f96ecaf9348da442e4be0711bd7ab
Martin <outofculture@gmail.com>
2020-08-15 22:15:58 -0700

    improve docstring formatting and wording

--- pyqtgraph/Vector.py
+++ pyqtgraph/Vector.py
@@ -17,14 +17,14 @@ class Vector(QtGui.QVector3D):
 
         ==============  ================================================================================================
         **Arguments:**
-        *args*      Could be any of:
-                     * 3 numerics
-                     * 2 numerics (`0` assumed for z)
-                     * Either of the previous in a list-like collection
-                     * 1 QSizeF (`0` assumed for z)
-                     * 1 QPointF (`0` assumed for z)
-                     * Any other valid QVector3D init args.
-        ----------------------------------------------------------------------------------------------------------------
+        *args*          Could be any of:
+
+                         * 3 numerics (x, y, and z)
+                         * 2 numerics (x, y, and `0` assumed for z)
+                         * Either of the previous in a list-like collection
+                         * 1 QSizeF (`0` assumed for z)
+                         * 1 QPointF (`0` assumed for z)
+                         * Any other valid QVector3D init args.
         ==============  ================================================================================================
         """
         initArgs = args
