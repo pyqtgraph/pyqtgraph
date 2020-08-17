52717fa8b7b6ce60ab2b6fc84c4a50bca5d8ea29
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-08-17 09:25:37 +0200

    Rename test

--- pyqtgraph/graphicsItems/tests/test_ArrowItem.py
+++ pyqtgraph/graphicsItems/tests/test_ArrowItem.py
@@ -3,7 +3,7 @@ import pyqtgraph as pg
 app = pg.mkQApp()
 
 
-def test_ArrowItem():
+def test_ArrowItem_parent():
     parent = pg.GraphicsObject()
     a = pg.ArrowItem(parent=parent, pos=(10, 10))
     assert a.parentItem() is parent
