05ee86c0ceeb409c7bed6f85226c42f8e693808a
Ogi Moore <ognyan.moore@gmail.com>
2020-10-19 09:28:00 -0700

    Non-relative import for example app

--- examples/test_ExampleApp.py
+++ examples/test_ExampleApp.py
@@ -1,7 +1,9 @@
+import initExample ## Add path to library (just for examples; you do not need this)
+
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 
-from .ExampleApp import ExampleLoader
+from pg.examples.ExampleApp import ExampleLoader
 
 loader = ExampleLoader()
 
