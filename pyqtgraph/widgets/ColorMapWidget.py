25ef6fa015690318d604b0b8520e9a5ac20ea8e8
ALLENINST\stephanies <stephanies@alleninstitute.org>
2020-11-05 10:59:18 -0800

    default colors for enum ColorMap type

--- pyqtgraph/widgets/ColorMapWidget.py
+++ pyqtgraph/widgets/ColorMapWidget.py
@@ -71,7 +71,15 @@ class ColorMapParameter(ptree.types.GroupParameter):
         defaults = fieldSpec.get('defaults', {})
         for k, v in defaults.items():
             if k == 'colormap':
-                item.setValue(v)
+                if mode == 'range':
+                    item.setValue(v)
+                if mode == 'enum':
+                    children = item.param('Values').children()
+                    for i, child in enumerate(children):
+                        try:
+                            child.setValue(v[i])
+                        except IndexError('No default color set for child %s' % child.name()):
+                            continue
             else:
                 item[k] = v
 
