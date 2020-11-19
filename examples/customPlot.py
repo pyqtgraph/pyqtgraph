ed384c71ceaab11536873779d69f3d4892371ee5
2xB <2xB@users.noreply.github.com>
2020-11-19 17:28:02 +0100

    Fix examples\customPlot.py: Avoid scaling differences
    Consider padding of TickSliderItem for link between ViewBox and ticks

--- examples/customPlot.py
+++ examples/customPlot.py
@@ -48,20 +48,27 @@ class CustomTickSliderItem(pg.TickSliderItem):
         self.updateRange(None, self._range)
     
     def updateRange(self, vb, viewRange):
+        origin = self.tickSize/2.
+        length = self.length
+
+        lengthIncludingPadding = length + self.tickSize + 2
+        
         self._range = viewRange
         
         for pos in self.all_ticks:
-            visible = pos >= viewRange[0] and pos <= viewRange[1]
+        
+            if pos not in self.visible_ticks:
+                self.visible_ticks[pos] = self.addTick(pos, movable=False, color="333333")
+            
+            tick = self.visible_ticks[pos]
+            
+            tickValueIncludingPadding = (pos - viewRange[0]) / (viewRange[1] - viewRange[0])
+            tickValue = (tickValueIncludingPadding*lengthIncludingPadding - origin) / length
+            
+            visible = tickValue >= 0 and tickValue <= 1
             
             if visible:
-                if pos not in self.visible_ticks:
-                    self.visible_ticks[pos] = self.addTick(pos, movable=False, color="333333")
-                
-                tick = self.visible_ticks[pos]
-                
-                tickValue = (pos - viewRange[0]) / (viewRange[1] - viewRange[0])
                 self.setTickValue(tick, tickValue)
-            
             elif pos in self.visible_ticks:
                 self.removeTick(self.visible_ticks[pos])
                 del self.visible_ticks[pos]
