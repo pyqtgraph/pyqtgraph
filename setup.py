ba61bbf8f7cb2c220d429c4e60b35381f283f936
Dennis Goeries <dennis.goeries@xfel.eu>
2021-01-11 20:17:11 +0100

    Include package data and remove username from svg

--- setup.py
+++ setup.py
@@ -139,7 +139,9 @@ setup(
               'style': helpers.StyleCommand},
     packages=allPackages,
     package_dir={'pyqtgraph.examples': 'examples'},  ## install examples along with the rest of the source
-    package_data={'pyqtgraph.examples': ['optics/*.gz', 'relativity/presets/*.cfg']},
+    package_data={'pyqtgraph.examples': ['optics/*.gz', 'relativity/presets/*.cfg'],
+                  "pyqtgraph.icons": ["*.*"],
+                  },
     install_requires = [
         'numpy>=1.8.0',
         ],
