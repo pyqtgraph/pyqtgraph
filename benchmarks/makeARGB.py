d5d31a2f453914c8315beb172d253b12eda1ca13
Martin <outofculture@gmail.com>
2021-01-19 13:32:36 -0800

    lint, avoid shadowing

--- benchmarks/makeARGB.py
+++ benchmarks/makeARGB.py
@@ -4,6 +4,14 @@ import pyqtgraph as pg
 
 
 class TimeSuite(object):
+    def __init__(self):
+        self.c_map = None
+        self.float_data = None
+        self.uint8_data = None
+        self.uint8_lut = None
+        self.uint16_data = None
+        self.uint16_lut = None
+
     def setup(self):
         size = (500, 500)
 
@@ -22,7 +30,7 @@ class TimeSuite(object):
             'levels': [20, 220],
         }
 
-        self.cmap = np.array([
+        self.c_map = np.array([
             [-500., 255.],
             [-255., 255.],
             [0., 500.],
@@ -30,32 +38,30 @@ class TimeSuite(object):
 
         self.uint8_lut = np.zeros((256, 4), dtype='ubyte')
         for i in range(3):
-            self.uint8_lut[:, i] = np.clip(np.linspace(self.cmap[i][0], self.cmap[i][1], 256), 0, 255)
+            self.uint8_lut[:, i] = np.clip(np.linspace(self.c_map[i][0], self.c_map[i][1], 256), 0, 255)
         self.uint8_lut[:, 3] = 255
 
         self.uint16_lut = np.zeros((2 ** 16, 4), dtype='ubyte')
         for i in range(3):
-            self.uint16_lut[:, i] = np.clip(np.linspace(self.cmap[i][0], self.cmap[i][1], 2 ** 16), 0, 255)
+            self.uint16_lut[:, i] = np.clip(np.linspace(self.c_map[i][0], self.c_map[i][1], 2 ** 16), 0, 255)
         self.uint16_lut[:, 3] = 255
 
 
-def make_test(*args):
+def make_test(dtype, use_levels, lut_name, func_name):
     def time_test(self):
-        data, levels, lut, name = args
-        data = getattr(self, data + '_data')
-        if lut is not None:
-            lut = getattr(self, lut + '_lut')
-        if levels is not None:
-            levels = data['levels']
-        data = data['data']
-        pg.makeARGB(data, levels=levels, lut=lut)
+        data = getattr(self, dtype + '_data')
+        pg.makeARGB(
+            data['data'],
+            lut=getattr(self, lut_name + '_lut', None),
+            levels=use_levels and data['levels'],
+        )
 
-    time_test.__name__ = name
+    time_test.__name__ = func_name
     return time_test
 
 
-for data in ['float', 'uint16', 'uint8']:
+for dt in ['float', 'uint16', 'uint8']:
     for levels in [True, False]:
-        for lut in [None, 'uint8', 'uint16']:
-            name = 'time_makeARGB_%s_%s_%slut' % (data, 'levels' if levels else 'nolevels', lut or 'no')
-            setattr(TimeSuite, name, make_test(data, levels, lut, name))
+        for ln in [None, 'uint8', 'uint16']:
+            name = f'time_makeARGB_{dt}_{"" if levels else "no"}levels_{ln or "no"}lut'
+            setattr(TimeSuite, name, make_test(dt, levels, ln, name))
