61de49081a83adabcc935cef348fe616bf7d80f8
Fekete Imre <feketeimre87@gmail.com>
2021-01-22 20:40:53 +0100

    Update pyqtgraph/widgets/PenSelectorDialogbox.py
    Co-authored-by: ChristophRose <42769515+ChristophRose@users.noreply.github.com>

--- pyqtgraph/widgets/PenSelectorDialogbox.py
+++ pyqtgraph/widgets/PenSelectorDialogbox.py
@@ -22,7 +22,9 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_Dialog(object):
+__all__ = ['PenSelectorDialogbox']
+
+class PenSelectorDialogbox(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(178, 280)
