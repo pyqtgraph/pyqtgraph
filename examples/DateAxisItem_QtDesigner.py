2486a74702b7bd99316ff6937a8c911e55b28768
KIU Shueng Chuan <nixchuan@gmail.com>
2021-01-26 08:44:16 +0800

    use mkQApp and don't set style nor palette
    not needed for an example and also avoids a PyQt6 6.0 refcount bug in
    app.setStyle()

--- examples/DateAxisItem_QtDesigner.py
+++ examples/DateAxisItem_QtDesigner.py
@@ -12,8 +12,8 @@ import time
 import os
 
 import numpy as np
-from pyqtgraph.Qt import QtCore, QtWidgets
 import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets, QtCore, loadUiType
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
@@ -22,7 +22,7 @@ BLUE = pg.mkPen('#1f77b4')
 
 path = os.path.dirname(os.path.abspath(__file__))
 uiFile = os.path.join(path, 'DateAxisItem_QtDesigner.ui')
-Design, _ = pg.Qt.loadUiType(uiFile)
+Design, _ = loadUiType(uiFile)
 
 class ExampleApp(QtWidgets.QMainWindow, Design):
     def __init__(self):
@@ -37,20 +37,7 @@ class ExampleApp(QtWidgets.QMainWindow, Design):
         self.plotWidget.setAxisItems({'bottom': pg.DateAxisItem()})
         self.plotWidget.showGrid(x=True, y=True)
 
-# Qt docs have the following warning regarding the following function:
-#   "To ensure that the application's style is set correctly, it is best
-#   to call this function before the QApplication constructor, if possible"
-QtWidgets.QApplication.setStyle('Fusion')
-app = QtWidgets.QApplication(sys.argv)
-if 0:
-    # PyQt6 6.0 seems to have a reference counting bug which will cause
-    # a crash
-    #   in PySide2, PySide6, PyQt5, we get refcount=3
-    #   in PyQt6, we get refcount=2
-    style = QtWidgets.QStyleFactory.create('Fusion')
-    app.setStyle(style)
-    print(sys.getrefcount(style))
-app.setPalette(QtWidgets.QApplication.style().standardPalette())
+app = pg.mkQApp("DateAxisItem_QtDesigner Example")
 window = ExampleApp()
 window.setWindowTitle('pyqtgraph example: DateAxisItem_QtDesigner')
 window.show()
