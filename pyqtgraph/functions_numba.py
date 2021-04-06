4b4d16e5f4be79a0a5a7c65a400b3946138746b3
KIU Shueng Chuan <nixchuan@gmail.com>
2021-04-07 07:39:52 +0800

    workaround to pass test_makeARGB.py

--- pyqtgraph/functions_numba.py
+++ pyqtgraph/functions_numba.py
@@ -3,21 +3,41 @@ import numba
 
 @numba.guvectorize(
     '(n),(),(),(),()->(n)', nopython=True)
-def rescale_clip(xx, scale, offset, vmin, vmax, yy):
+def rescale_clip_uint8(xx, scale, offset, vmin, vmax, yy):
     for i in range(xx.size):
         val = (xx[i] - offset) * scale
         yy[i] = min(max(val, vmin), vmax)
 
 @numba.guvectorize(
     '(n),(),()->(n)', nopython=True)
-def rescale_noclip(xx, scale, offset, yy):
+def rescale_noclip_uint8(xx, scale, offset, yy):
+    for i in range(xx.size):
+        yy[i] = (xx[i] - offset) * scale
+
+@numba.guvectorize(
+    '(n),(),(),(),()->(n)', nopython=True)
+def rescale_clip_uint16(xx, scale, offset, vmin, vmax, yy):
+    for i in range(xx.size):
+        val = (xx[i] - offset) * scale
+        yy[i] = min(max(val, vmin), vmax)
+
+@numba.guvectorize(
+    '(n),(),()->(n)', nopython=True)
+def rescale_noclip_uint16(xx, scale, offset, yy):
     for i in range(xx.size):
         yy[i] = (xx[i] - offset) * scale
 
 
 def rescaleData(data, scale, offset, dtype, clip):
     data_out = np.empty_like(data, dtype=dtype)
-    if clip is None:
-        return rescale_noclip(data, scale, offset, out=data_out)
+    if dtype == np.uint8:
+        if clip is None:
+            rescale_noclip_uint8(data, scale, offset, out=data_out)
+        else:
+            rescale_clip_uint8(data, scale, offset, clip[0], clip[1], out=data_out)
     else:
-        return rescale_clip(data, scale, offset, clip[0], clip[1], out=data_out)
+        if clip is None:
+            rescale_noclip_uint16(data, scale, offset, out=data_out)
+        else:
+            rescale_clip_uint16(data, scale, offset, clip[0], clip[1], out=data_out)
+    return data_out
