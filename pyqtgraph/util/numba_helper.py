db27c966aef68c026545aeef5d98583060cb8343
KIU Shueng Chuan <nixchuan@gmail.com>
2021-04-08 07:27:06 +0800

    remove minimum version check

--- pyqtgraph/util/numba_helper.py
+++ pyqtgraph/util/numba_helper.py
@@ -10,10 +10,6 @@ def getNumbaFunctions():
             warn("numba library could not be loaded, but 'useNumba' is set.")
             return None
 
-        if numba.version_info.full < (0, 53):
-            warn(f"pyqtgraph requires numba version >= 0.53  (your version is {numba.version_info.string})")
-            return None
-
         from .. import functions_numba
         return functions_numba
     else:
