299e80222319a1a7495788a91b579c9f227d4a9e
KIU Shueng Chuan <nixchuan@gmail.com>
2021-04-16 08:09:40 +0800

    add numba test to test_makeARGB.py

--- pyqtgraph/tests/test_makeARGB.py
+++ pyqtgraph/tests/test_makeARGB.py
@@ -4,7 +4,7 @@ import pytest
 import sys
 from typing import Dict, Any, Union, Type
 
-from pyqtgraph import getCupy, setConfigOption
+from pyqtgraph import getCupy, getConfigOption, setConfigOption
 from pyqtgraph.functions import makeARGB as real_makeARGB
 
 IN_2D_INT8 = np.array([[173, 48, 122, 41], [210, 192, 0, 5], [104, 56, 102, 115], [78, 19, 255, 6]], dtype=np.uint8)
@@ -4316,6 +4316,18 @@ def test_cupy_makeARGB_against_generated_references():
             ).all(), f"Incorrect _makeARGB({key!r}) output! Expected:\n{expectation!r}\n  Got:\n{output!r}"
 
     _do_something_for_every_combo(assert_cupy_correct)
+
+
+def test_numba_makeARGB_against_generated_references():
+    try:
+        import numba
+    except ImportError:
+        pytest.skip("Numba unavailable to test")
+
+    oldcfg = getConfigOption("useNumba")
+    setConfigOption("useNumba", not oldcfg)
+    test_makeARGB_against_generated_references()
+    setConfigOption("useNumba", oldcfg)
 
 
 def test_makeARGB_with_human_readable_code():
