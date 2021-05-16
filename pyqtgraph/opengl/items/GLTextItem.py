f0f9e01bc78fe4bd9f001e1290c0a7ab78829c4e
shikishima <160442065@ccalumni.meijo-u.ac.jp>
2021-05-16 17:20:56 +0900

    [The long code has been broken up into separate lines.](https://github.com/pyqtgraph/pyqtgraph/pull/1776#discussion_r633046234)

--- pyqtgraph/opengl/items/GLTextItem.py
+++ pyqtgraph/opengl/items/GLTextItem.py
@@ -58,8 +58,23 @@ class GLTextItem(GLGraphicsItem):
                         if len(value) != 4:
                             raise Exception('"len(color)" must be 4.')
                 elif arg == 'font':
-                    if value not in [GLUT_BITMAP_8_BY_13, GLUT_BITMAP_9_BY_15, GLUT_BITMAP_TIMES_ROMAN_10, GLUT_BITMAP_TIMES_ROMAN_24, GLUT_BITMAP_HELVETICA_10, GLUT_BITMAP_HELVETICA_12, GLUT_BITMAP_HELVETICA_18]:
-                        raise Exception('"font" must be "GLUT_BITMAP_8_BY_13", "GLUT_BITMAP_9_BY_15", "GLUT_BITMAP_TIMES_ROMAN_10", "GLUT_BITMAP_TIMES_ROMAN_24", "GLUT_BITMAP_HELVETICA_10", "GLUT_BITMAP_HELVETICA_12", or "GLUT_BITMAP_HELVETICA_18".')
+                    if value not in [
+                        GLUT_BITMAP_8_BY_13,
+                        GLUT_BITMAP_9_BY_15,
+                        GLUT_BITMAP_TIMES_ROMAN_10,
+                        GLUT_BITMAP_TIMES_ROMAN_24,
+                        GLUT_BITMAP_HELVETICA_10,
+                        GLUT_BITMAP_HELVETICA_12,
+                        GLUT_BITMAP_HELVETICA_18
+                    ]:
+                        raise Exception('"font" must be ' \
+                            '"GLUT_BITMAP_8_BY_13", ' \
+                            '"GLUT_BITMAP_9_BY_15", ' \
+                            '"GLUT_BITMAP_TIMES_ROMAN_10", ' \
+                            '"GLUT_BITMAP_TIMES_ROMAN_24", ' \
+                            '"GLUT_BITMAP_HELVETICA_10", ' \
+                            '"GLUT_BITMAP_HELVETICA_12", or ' \
+                            '"GLUT_BITMAP_HELVETICA_18".')
                 setattr(self, arg, value)
         self.update()
     
