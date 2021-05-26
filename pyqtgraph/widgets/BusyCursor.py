3f59ec6a2df8516d4f4e8cf6f18e123a3e4a57ca
Martin Chase <outofculture@gmail.com>
2021-05-26 19:48:14 -0400

    un-busy as many times as needed

--- pyqtgraph/widgets/BusyCursor.py
+++ pyqtgraph/widgets/BusyCursor.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from ..Qt import QtGui, QtCore, QT_LIB
 
 __all__ = ['BusyCursor']
+
 
 class BusyCursor(object):
     """Class for displaying a busy mouse cursor during long operations.
@@ -12,6 +14,7 @@ class BusyCursor(object):
     May be nested. If called from a non-gui thread, then the cursor will not be affected.
     """
     active = []
+    nesting_count = 0
 
     def __enter__(self):
         app = QtCore.QCoreApplication.instance()
@@ -24,6 +27,7 @@ class BusyCursor(object):
             else:
                 QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
             BusyCursor.active.append(self)
+            BusyCursor.nesting_count += 1
             self._active = True
         else:
             self._active = False
@@ -32,4 +36,6 @@ class BusyCursor(object):
         if self._active:
             BusyCursor.active.pop(-1)
             if len(BusyCursor.active) == 0:
-                QtGui.QApplication.restoreOverrideCursor()
+                for _ in range(BusyCursor.nesting_count):
+                    QtGui.QApplication.restoreOverrideCursor()
+                BusyCursor.nesting_count = 0
