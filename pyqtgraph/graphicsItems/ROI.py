1735effea73d051c3155e6d261cf0c3f778f303f
Martin Chase <outofculture@gmail.com>
2021-05-26 17:14:08 -0700

    fix click bug on removable ROIs; test (#1804)

--- pyqtgraph/graphicsItems/ROI.py
+++ pyqtgraph/graphicsItems/ROI.py
@@ -801,7 +801,7 @@ class ROI(GraphicsObject):
         if ev.button() == QtCore.Qt.RightButton and self.contextMenuEnabled():
             self.raiseContextMenu(ev)
             ev.accept()
-        elif ev.button() in self.acceptedMouseButtons():
+        elif ev.button() & self.acceptedMouseButtons() > 0:
             ev.accept()
             self.sigClicked.emit(self, ev)
         else:
