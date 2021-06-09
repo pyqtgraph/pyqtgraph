a97931bbf1fec3a35ee3fedcc643380d5dea7ce4
Martin Chase <outofculture@gmail.com>
2021-06-09 10:35:23 -0700

    use contextmanager decorator instead of class

--- pyqtgraph/widgets/BusyCursor.py
+++ pyqtgraph/widgets/BusyCursor.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
-from ..Qt import QtGui, QtCore, QT_LIB
+from contextlib import contextmanager
+
+from ..Qt import QtGui, QtCore
 
 __all__ = ["BusyCursor"]
 
 
-class BusyCursor(object):
-    """Class for displaying a busy mouse cursor during long operations.
-    Usage::
-
-        with pyqtgraph.BusyCursor():
-            doLongOperation()
-
-    May be nested. If called from a non-gui thread, then the cursor will not be affected.
-    """
-
-    def __enter__(self):
-        app = QtCore.QCoreApplication.instance()
-        self._active = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
-        if self._active:
+@contextmanager
+def BusyCursor():
+    app = QtCore.QCoreApplication.instance()
+    active = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
+    try:
+        if active:
             QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
-
-    def __exit__(self, *args):
-        if self._active:
+        yield
+    finally:
+        if active:
             QtGui.QApplication.restoreOverrideCursor()
