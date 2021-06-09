eb1bcabcbb06ef350e8e0e43a8ee79cb648b9487
Martin Chase <outofculture@gmail.com>
2021-06-09 10:48:34 -0700

    fix docstring whitespace

--- pyqtgraph/widgets/BusyCursor.py
+++ pyqtgraph/widgets/BusyCursor.py
@@ -11,8 +11,10 @@ def BusyCursor():
     """
     Display a busy mouse cursor during long operations.
     Usage::
+
         with BusyCursor():
             doLongOperation()
+
     May be nested. If called from a non-gui thread, then the cursor will not be affected.
     """
     app = QtCore.QCoreApplication.instance()
