d6e257b6c90f11ebafaba62b28488db00b06600e
Martin Chase <outofculture@gmail.com>
2021-06-09 10:50:40 -0700

    break up long lines

--- tests/widgets/test_busycursor.py
+++ tests/widgets/test_busycursor.py
@@ -7,6 +7,10 @@ pg.mkQApp()
 def test_nested_busy_cursors_clear_after_all_exit():
     with pg.BusyCursor():
         with pg.BusyCursor():
-            assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor, "Cursor should be waiting"
-        assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor, "Cursor should be waiting"
+            assert (
+                pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor
+            ), "Cursor should be waiting"
+        assert (
+            pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor
+        ), "Cursor should be waiting"
     assert pg.Qt.QtGui.QApplication.overrideCursor() is None, "No override cursor should be set"
