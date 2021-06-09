07180432508ded8793e4bb6f0ed249fe1527fc3f
Martin Chase <outofculture@gmail.com>
2021-06-09 10:51:35 -0700

    use variable to shorten instead

--- tests/widgets/test_busycursor.py
+++ tests/widgets/test_busycursor.py
@@ -6,11 +6,8 @@ pg.mkQApp()
 
 def test_nested_busy_cursors_clear_after_all_exit():
     with pg.BusyCursor():
+        wait_cursor = pg.Qt.QtCore.Qt.CursorShape.WaitCursor
         with pg.BusyCursor():
-            assert (
-                pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor
-            ), "Cursor should be waiting"
-        assert (
-            pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.CursorShape.WaitCursor
-        ), "Cursor should be waiting"
+            assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == wait_cursor, "Cursor should be waiting"
+        assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == wait_cursor, "Cursor should be waiting"
     assert pg.Qt.QtGui.QApplication.overrideCursor() is None, "No override cursor should be set"
