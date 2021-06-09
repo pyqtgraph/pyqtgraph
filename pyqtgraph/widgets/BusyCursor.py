53c7e2ae0c667553517a2357c3936db5ea71ec82
Martin Chase <outofculture@gmail.com>
2021-06-09 10:29:13 -0700

    tentative change in the hopes that all supported qt versions behave properly

--- pyqtgraph/widgets/BusyCursor.py
+++ pyqtgraph/widgets/BusyCursor.py
@@ -19,24 +19,10 @@ class BusyCursor(object):
 
     def __enter__(self):
         app = QtCore.QCoreApplication.instance()
-        isGuiThread = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
-        if isGuiThread and QtGui.QApplication.instance() is not None:
-            if QT_LIB == "PySide":
-                # pass CursorShape rather than QCursor for PySide
-                # see https://bugreports.qt.io/browse/PYSIDE-243
-                QtGui.QApplication.setOverrideCursor(QtCore.Qt.CursorShape.WaitCursor)
-            else:
-                QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
-            BusyCursor.active.append(self)
-            BusyCursor.nesting_count += 1
-            self._active = True
-        else:
-            self._active = False
+        self._active = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
+        if self._active:
+            QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
 
     def __exit__(self, *args):
         if self._active:
-            BusyCursor.active.pop(-1)
-            if len(BusyCursor.active) == 0:
-                for _ in range(BusyCursor.nesting_count):
-                    QtGui.QApplication.restoreOverrideCursor()
-                BusyCursor.nesting_count = 0
+            QtGui.QApplication.restoreOverrideCursor()
