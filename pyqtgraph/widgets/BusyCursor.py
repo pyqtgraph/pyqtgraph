26b3d6b4111400d141691be54e8f0b2dbf5bfa03
Martin Chase <outofculture@gmail.com>
2021-06-09 10:42:35 -0700

    restore docstring; refactor variable for clarity

--- pyqtgraph/widgets/BusyCursor.py
+++ pyqtgraph/widgets/BusyCursor.py
@@ -8,12 +8,19 @@ __all__ = ["BusyCursor"]
 
 @contextmanager
 def BusyCursor():
+    """
+    Display a busy mouse cursor during long operations.
+    Usage::
+        with BusyCursor():
+            doLongOperation()
+    May be nested. If called from a non-gui thread, then the cursor will not be affected.
+    """
     app = QtCore.QCoreApplication.instance()
-    active = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
+    in_gui_thread = (app is not None) and (QtCore.QThread.currentThread() == app.thread())
     try:
-        if active:
+        if in_gui_thread:
             QtGui.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CursorShape.WaitCursor))
         yield
     finally:
-        if active:
+        if in_gui_thread:
             QtGui.QApplication.restoreOverrideCursor()
