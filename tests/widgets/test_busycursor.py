98194f4bb46fef1ac6a6a810774bf5ef54ef3d92
Martin Chase <outofculture@gmail.com>
2021-06-09 10:27:01 -0700

    add test to prove cursor behavior

---
+++ tests/widgets/test_busycursor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pyqtgraph as pg
+
+pg.mkQApp()
+
+
+def test_nested_busy_cursors_clear_after_all_exit():
+    with pg.BusyCursor():
+        with pg.BusyCursor():
+            assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.WaitCursor, "Cursor should be waiting"
+        assert pg.Qt.QtGui.QApplication.overrideCursor().shape() == pg.Qt.QtCore.Qt.WaitCursor, "Cursor should be waiting"
+    assert pg.Qt.QtGui.QApplication.overrideCursor() is None, "No override cursor should be set"
