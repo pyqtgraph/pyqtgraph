bcb8183a490b2689d7dc697662146d35a80b126c
njessurun <ntjessu@gmail.com>
2021-07-18 17:14:55 -0400

    Adds parameter descriptions to docstrings

--- pyqtgraph/parametertree/parameterTypes.py
+++ pyqtgraph/parametertree/parameterTypes.py
@@ -945,18 +945,6 @@ class ProgressBarParameter(Parameter):
     itemClass = ProgressBarParameterItem
 
 class SliderParameterItem(WidgetParameterItem):
-    """
-    ============== ========================================================
-    **Arguments:**
-    limits         [start, stop] numbers
-    step:          Defaults to 1, the spacing between each slider tick
-    span:          Instead of limits + step, span can be set to specify
-                   the range of slider options (e.g. np.linspace(-pi, pi, 100))
-    format:        Format string to determine number of decimals to show, etc.
-                   Defaults to display based on span dtype
-    precision:     int number of decimals to keep for float tick spaces
-    ============== ========================================================
-    """
     slider: QtWidgets.QSlider
     span: np.ndarray
     charSpan: np.ndarray
@@ -1062,6 +1050,18 @@ class SliderParameterItem(WidgetParameterItem):
         self.optsChanged(param, dict(limits=limits))
 
 class SliderParameter(Parameter):
+    """
+    ============== ========================================================
+    **Arguments:**
+    limits         [start, stop] numbers
+    step:          Defaults to 1, the spacing between each slider tick
+    span:          Instead of limits + step, span can be set to specify
+                   the range of slider options (e.g. np.linspace(-pi, pi, 100))
+    format:        Format string to determine number of decimals to show, etc.
+                   Defaults to display based on span dtype
+    precision:     int number of decimals to keep for float tick spaces
+    ============== ========================================================
+    """
     itemClass = SliderParameterItem
 
 class FontParameterItem(WidgetParameterItem):
