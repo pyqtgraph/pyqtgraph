c80eac3ec44826aed4f1a83061356a59c85c47c7
njessurun <ntjessu@gmail.com>
2021-07-23 08:43:42 -0400

    progress, file, slider

--- pyqtgraph/parametertree/parameterTypes.py
+++ pyqtgraph/parametertree/parameterTypes.py
@@ -749,13 +749,13 @@ class TextParameter(Parameter):
 registerParameterType('text', TextParameter, override=True)
 
 
-# class Emitter(QtCore.QObject):
-#     """
-#     WidgetParameterItem is not a QObject, and the slider's value needs to be converted before
-#     emitting. So, create an emitter class here that can be used instead
-#     """
-#     sigChanging = QtCore.Signal(object, object)
-#     sigChanged = QtCore.Signal(object, object)
+class Emitter(QtCore.QObject):
+    """
+    WidgetParameterItem is not a QObject, and the slider's value needs to be converted before
+    emitting. So, create an emitter class here that can be used instead
+    """
+    sigChanging = QtCore.Signal(object, object)
+    sigChanged = QtCore.Signal(object, object)
 
 
 def _set_filepicker_kwargs(fileDlg, **kwargs):
@@ -970,125 +970,125 @@ class ProgressBarParameter(Parameter):
     """
     itemClass = ProgressBarParameterItem
 
-# class SliderParameterItem(WidgetParameterItem):
-#     slider: QtWidgets.QSlider
-#     span: np.ndarray
-#     charSpan: np.ndarray
+class SliderParameterItem(WidgetParameterItem):
+    slider: QtWidgets.QSlider
+    span: np.ndarray
+    charSpan: np.ndarray
 
-#     def __init__(self, param, depth):
-#         # Bind emitter to self to avoid garbage collection
-#         self.emitter = Emitter()
-#         self.sigChanging = self.emitter.sigChanging
-#         self._suffix = None
-#         super().__init__(param, depth)
+    def __init__(self, param, depth):
+        # Bind emitter to self to avoid garbage collection
+        self.emitter = Emitter()
+        self.sigChanging = self.emitter.sigChanging
+        self._suffix = None
+        super().__init__(param, depth)
 
-#     def updateDisplayLabel(self, value=None):
-#         if value is None:
-#             value = self.param.value()
-#         value = asUnicode(value)
-#         if self._suffix is None:
-#             suffixTxt = ''
-#         else:
-#             suffixTxt = f' {self._suffix}'
-#         self.displayLabel.setText(value + suffixTxt)
+    def updateDisplayLabel(self, value=None):
+        if value is None:
+            value = self.param.value()
+        value = asUnicode(value)
+        if self._suffix is None:
+            suffixTxt = ''
+        else:
+            suffixTxt = f' {self._suffix}'
+        self.displayLabel.setText(value + suffixTxt)
 
-#     def setSuffix(self, suffix):
-#         self._suffix = suffix
-#         self._updateLabel(self.slider.value())
+    def setSuffix(self, suffix):
+        self._suffix = suffix
+        self._updateLabel(self.slider.value())
 
-#     def makeWidget(self):
-#         param = self.param
-#         opts = param.opts
-#         self._suffix = opts.get('suffix')
+    def makeWidget(self):
+        param = self.param
+        opts = param.opts
+        self._suffix = opts.get('suffix')
 
-#         self.slider = QtWidgets.QSlider()
-#         self.slider.setOrientation(QtCore.Qt.Orientation.Horizontal)
-#         lbl = QtWidgets.QLabel()
-#         lbl.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.slider = QtWidgets.QSlider()
+        self.slider.setOrientation(QtCore.Qt.Orientation.Horizontal)
+        lbl = QtWidgets.QLabel()
+        lbl.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
 
-#         w = QtWidgets.QWidget()
-#         layout = QtWidgets.QHBoxLayout()
-#         w.setLayout(layout)
-#         layout.addWidget(lbl)
-#         layout.addWidget(self.slider)
+        w = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+        w.setLayout(layout)
+        layout.addWidget(lbl)
+        layout.addWidget(self.slider)
 
-#         def setValue(v):
-#             self.slider.setValue(self.spanToSliderValue(v))
-#         def getValue():
-#             return self.span[self.slider.value()].item()
+        def setValue(v):
+            self.slider.setValue(self.spanToSliderValue(v))
+        def getValue():
+            return self.span[self.slider.value()].item()
 
-#         def vChanged(v):
-#             lbl.setText(self.prettyTextValue(v))
-#         self.slider.valueChanged.connect(vChanged)
+        def vChanged(v):
+            lbl.setText(self.prettyTextValue(v))
+        self.slider.valueChanged.connect(vChanged)
 
-#         def onMove(pos):
-#             self.sigChanging.emit(self, self.span[pos].item())
-#         self.slider.sliderMoved.connect(onMove)
+        def onMove(pos):
+            self.sigChanging.emit(self, self.span[pos].item())
+        self.slider.sliderMoved.connect(onMove)
 
-#         w.setValue = setValue
-#         w.value = getValue
-#         w.sigChanged = self.slider.valueChanged
-#         w.sigChanging = self.sigChanging
-#         self.optsChanged(param, opts)
-#         return w
+        w.setValue = setValue
+        w.value = getValue
+        w.sigChanged = self.slider.valueChanged
+        w.sigChanging = self.sigChanging
+        self.optsChanged(param, opts)
+        return w
 
-#     # def updateDisplayLabel(self, value=None):
-#     #   self.displayLabel.setText(self.prettyTextValue(value))
+    # def updateDisplayLabel(self, value=None):
+    #   self.displayLabel.setText(self.prettyTextValue(value))
 
-#     def spanToSliderValue(self, v):
-#         return int(np.argmin(np.abs(self.span-v)))
+    def spanToSliderValue(self, v):
+        return int(np.argmin(np.abs(self.span-v)))
 
-#     def prettyTextValue(self, v):
-#         if self._suffix is None:
-#             suffixTxt = ''
-#         else:
-#             suffixTxt = f' {self._suffix}'
-#         format_ = self.param.opts.get('format', None)
-#         cspan = self.charSpan
-#         if format_ is None:
-#             format_ = f'{{0:>{cspan.dtype.itemsize}}}{suffixTxt}'
-#         return format_.format(cspan[v].decode())
+    def prettyTextValue(self, v):
+        if self._suffix is None:
+            suffixTxt = ''
+        else:
+            suffixTxt = f' {self._suffix}'
+        format_ = self.param.opts.get('format', None)
+        cspan = self.charSpan
+        if format_ is None:
+            format_ = f'{{0:>{cspan.dtype.itemsize}}}{suffixTxt}'
+        return format_.format(cspan[v].decode())
 
-#     def optsChanged(self, param, opts):
-#         try:
-#             super().optsChanged(param, opts)
-#         except AttributeError as ex:
-#             pass
-#         span = opts.get('span', None)
-#         if span is None:
-#             step = opts.get('step', 1)
-#             start, stop = opts['limits']
-#             # Add a bit to 'stop' since python slicing excludes the last value
-#             span = np.arange(start, stop+step, step)
-#         precision = opts.get('precision', 2)
-#         if precision is not None:
-#             span = span.round(precision)
-#         self.span = span
-#         self.charSpan = np.char.array(span)
-#         w = self.slider
-#         w.setMinimum(0)
-#         w.setMaximum(len(span)-1)
-#         if 'suffix' in opts:
-#             self.setSuffix(opts['suffix'])
-#             self.slider.valueChanged.emit(self.slider.value())
+    def optsChanged(self, param, opts):
+        try:
+            super().optsChanged(param, opts)
+        except AttributeError as ex:
+            pass
+        span = opts.get('span', None)
+        if span is None:
+            step = opts.get('step', 1)
+            start, stop = opts['limits']
+            # Add a bit to 'stop' since python slicing excludes the last value
+            span = np.arange(start, stop+step, step)
+        precision = opts.get('precision', 2)
+        if precision is not None:
+            span = span.round(precision)
+        self.span = span
+        self.charSpan = np.char.array(span)
+        w = self.slider
+        w.setMinimum(0)
+        w.setMaximum(len(span)-1)
+        if 'suffix' in opts:
+            self.setSuffix(opts['suffix'])
+            self.slider.valueChanged.emit(self.slider.value())
 
-#     def limitsChanged(self, param, limits):
-#         self.optsChanged(param, dict(limits=limits))
+    def limitsChanged(self, param, limits):
+        self.optsChanged(param, dict(limits=limits))
 
-# class SliderParameter(Parameter):
-#     """
-#     ============== ========================================================
-#     **Options**
-#     limits         [start, stop] numbers
-#     step:          Defaults to 1, the spacing between each slider tick
-#     span:          Instead of limits + step, span can be set to specify
-#                    the range of slider options (e.g. np.linspace(-pi, pi, 100))
-#     format:        Format string to determine number of decimals to show, etc.
-#                    Defaults to display based on span dtype
-#     precision:     int number of decimals to keep for float tick spaces
-#     ============== ========================================================
-#     """
-#     itemClass = SliderParameterItem
+class SliderParameter(Parameter):
+    """
+    ============== ========================================================
+    **Options**
+    limits         [start, stop] numbers
+    step:          Defaults to 1, the spacing between each slider tick
+    span:          Instead of limits + step, span can be set to specify
+                   the range of slider options (e.g. np.linspace(-pi, pi, 100))
+    format:        Format string to determine number of decimals to show, etc.
+                   Defaults to display based on span dtype
+    precision:     int number of decimals to keep for float tick spaces
+    ============== ========================================================
+    """
+    itemClass = SliderParameterItem
 
 # class FontParameterItem(WidgetParameterItem):
 #     def makeWidget(self):
@@ -1378,6 +1378,6 @@ class ProgressBarParameter(Parameter):
 # registerParameterType('pen', PenParameter, override=True)
 registerParameterType('progress', ProgressBarParameter, override=True)
 registerParameterType('file', FileParameter, override=True)
-# registerParameterType('slider', SliderParameter, override=True)
+registerParameterType('slider', SliderParameter, override=True)
 # registerParameterType('calendar', CalendarParameter, override=True)
 # registerParameterType('font', FontParameter, override=True)
