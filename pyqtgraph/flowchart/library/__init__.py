26782877f0cf794fa3fbdc45d3ddeac7418379ee
Martin Chase <outofculture@gmail.com>
2021-11-12 23:45:54 -0800

    this __init__.py legitimately does have unused imports

--- pyqtgraph/flowchart/library/__init__.py
+++ pyqtgraph/flowchart/library/__init__.py
@@ -1,10 +1,5 @@
-import os
-import types
-from collections import OrderedDict
-
-from ... import reload as reload
-from ...debug import printExc
 from ..NodeLibrary import NodeLibrary, isNodeClass
+from . import Data, Display, Filters, Operators
 
 # Build default library
 LIBRARY = NodeLibrary()
@@ -17,8 +12,6 @@ getNodeTree = LIBRARY.getNodeTree
 getNodeType = LIBRARY.getNodeType
 
 # Add all nodes to the default library
-from . import Data, Display, Filters, Operators
-
 for mod in [Data, Display, Filters, Operators]:
     nodes = [getattr(mod, name) for name in dir(mod) if isNodeClass(getattr(mod, name))]
     for node in nodes:
