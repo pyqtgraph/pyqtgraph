ab90f5ca1d8d9dc5f9c3d094bfdec023808f05d8
njessurun <ntjessu@gmail.com>
2021-11-13 22:20:08 -0500

    Use GroupParameter instead of popup button for PenParameter

--- pyqtgraph/parametertree/parameterTypes/pen.py
+++ pyqtgraph/parametertree/parameterTypes/pen.py
@@ -1,79 +1,50 @@
+import re
+from contextlib import ExitStack
+
+from . import GroupParameterItem
+from .basetypes import GroupParameter, Parameter, ParameterItem
+from .qtenum import QtEnumParameter
 from ... import functions as fn
-from ...Qt import QtCore, QtGui, QtWidgets
-from ...widgets.PenSelectorDialog import PenSelectorDialog
-from ..Parameter import Parameter
-from .basetypes import WidgetParameterItem
+from ...Qt import QtCore, QtWidgets, QtGui
 
-
-class PenParameterItem(WidgetParameterItem):
-    def __init__(self, param, depth):
-        self.pdialog = PenSelectorDialog(fn.mkPen(param.pen))
-        self.pdialog.setModal(True)
-        self.pdialog.accepted.connect(self.penChangeFinished)
-        super().__init__(param, depth)
-        self.displayLabel.paintEvent = self.displayPaintEvent
-
-    def makeWidget(self):
-        self.button = QtWidgets.QPushButton()
-        #larger button
-        self.button.setFixedWidth(100)
-        self.button.clicked.connect(self.buttonClicked)
-        self.button.paintEvent = self.buttonPaintEvent
-        self.button.value = self.value
-        self.button.setValue = self.setValue
-        self.button.sigChanged = None
-        return self.button
+class PenPreviewLabel(QtWidgets.QLabel):
+    def __init__(self, param):
+        super().__init__()
+        self.param = param
+        param.sigValueChanged.connect(self.update)
 
     @property
     def pen(self):
-        return self.pdialog.pen
+        return self.param.pen
 
-    def value(self):
-        return self.pen
-
-    def setValue(self, pen):
-        self.pdialog.updateParamFromPen(self.pdialog.param, pen)
-
-    def updateDisplayLabel(self, value=None):
-        super().updateDisplayLabel('')
-        self.displayLabel.update()
-        self.widget.update()
-
-    def buttonClicked(self):
-        #open up the pen selector dialog
-        # Copy in case of rejection
-        prePen = QtGui.QPen(self.pen)
-        if self.pdialog.exec() != QtWidgets.QDialog.DialogCode.Accepted:
-            self.pdialog.updateParamFromPen(self.pdialog.param, prePen)
-
-    def penChangeFinished(self):
-        self.param.setValue(self.pdialog.pen)
-
-    def penPaintEvent(self, event, item):
-        # draw item as usual
-        type(item).paintEvent(item, event)
-
+    def paintEvent(self, ev):
         path = QtGui.QPainterPath()
-        displaySize = item.size()
+        displaySize = self.size()
         w, h = displaySize.width(), displaySize.height()
         # draw a squiggle with the pen
         path.moveTo(w * .2, h * .2)
         path.lineTo(w * .4, h * .8)
         path.cubicTo(w * .5, h * .1, w * .7, h * .1, w * .8, h * .8)
 
-        painter = QtGui.QPainter(item)
+        painter = QtGui.QPainter(self)
         painter.setPen(self.pen)
         painter.drawPath(path)
         painter.end()
 
-    def buttonPaintEvent(self, event):
-        return self.penPaintEvent(event, self.button)
+class PenParameterItem(GroupParameterItem):
+    def __init__(self, param, depth):
+        super().__init__(param, depth)
+        self.penLabel = PenPreviewLabel(param)
 
-    def displayPaintEvent(self, event):
-        return self.penPaintEvent(event, self.displayLabel)
+    def treeWidgetChanged(self):
+        ParameterItem.treeWidgetChanged(self)
+        tw = self.treeWidget()
+        if tw is None:
+            return
+        tw.setItemWidget(self, 1, self.penLabel
+                         )
 
-
-class PenParameter(Parameter):
+class PenParameter(GroupParameter):
     """
     Controls the appearance of a QPen value.
 
@@ -89,20 +60,23 @@ class PenParameter(Parameter):
     cosmetic       Boolean, whether or not the pen is cosmetic (defaults to True)
     ============== ========================================================
     """
-
     itemClass = PenParameterItem
-    sigPenChanged = QtCore.Signal(object,object)
 
     def __init__(self, **opts):
         self.pen = fn.mkPen()
-        self.penOptsParam = PenSelectorDialog.mkParam(self.pen)
-        super().__init__(**opts)
+        children = self._makeChildren(self.pen)
+        if 'children' in opts:
+            raise KeyError('Cannot set "children" argument in Pen Parameter opts')
+        super().__init__(**opts, children=list(children))
 
     def saveState(self, filter=None):
         state = super().saveState(filter)
-        overrideState = self.penOptsParam.saveState(filter)['children']
-        state['value'] = tuple(s['value'] for s in overrideState.values())
+        opts = state.pop('children')
+        state['value'] = tuple(o['value'] for o in opts.values())
         return state
+
+    def restoreState(self, state, recursive=True, addChildren=True, removeChildren=True, blockSignals=True):
+        return super().restoreState(state, recursive=False, addChildren=False, removeChildren=False, blockSignals=blockSignals)
 
     def _interpretValue(self, v):
         return self.mkPen(v)
@@ -110,22 +84,22 @@ class PenParameter(Parameter):
     def setValue(self, value, blockSignal=None):
         if not fn.eq(value, self.pen):
             value = self.mkPen(value)
-            PenSelectorDialog.updateParamFromPen(self.penOptsParam, value)
+            self.updateFromPen(self, value)
         return super().setValue(self.pen, blockSignal)
 
     def applyOptsToPen(self, **opts):
         # Transform opts into a value for the current pen
-        paramNames = set(opts).intersection(self.penOptsParam.names)
+        paramNames = set(opts).intersection(self.names)
         # Value should be overridden by opts
         with self.treeChangeBlocker():
             if 'value' in opts:
                 pen = self.mkPen(opts.pop('value'))
                 if not fn.eq(pen, self.pen):
-                    PenSelectorDialog.updateParamFromPen(self.penOptsParam, pen)
+                    self.updateFromPen(self, pen)
             penOpts = {}
             for kk in paramNames:
                 penOpts[kk] = opts[kk]
-                self.penOptsParam[kk] = opts[kk]
+                self[kk] = opts[kk]
         return penOpts
 
     def setOpts(self, **opts):
@@ -137,9 +111,87 @@ class PenParameter(Parameter):
 
     def mkPen(self, *args, **kwargs):
         """Thin wrapper around fn.mkPen which accepts the serialized state from saveState"""
-        if len(args) == 1 and isinstance(args[0], tuple) and len(args[0]) == len(self.penOptsParam.childs):
-            opts = dict(zip(self.penOptsParam.names, args[0]))
+        if len(args) == 1 and isinstance(args[0], tuple) and len(args[0]) == len(self.childs):
+            opts = dict(zip(self.names, args[0]))
             self.applyOptsToPen(**opts)
             args = (self.pen,)
             kwargs = {}
         return fn.mkPen(*args, **kwargs)
+
+    def _makeChildren(self, boundPen=None):
+        cs = QtCore.Qt.PenCapStyle
+        js = QtCore.Qt.PenJoinStyle
+        ps = QtCore.Qt.PenStyle
+        param = Parameter.create(
+            name='Params', type='group', children=[
+                dict(name='color', type='color', value='k'),
+                dict(name='width', value=1, type='int', limits=[0, None]),
+                QtEnumParameter(ps, name='style', value='SolidLine'),
+                QtEnumParameter(cs, name='capStyle'),
+                QtEnumParameter(js, name='joinStyle'),
+                dict(name='cosmetic', type='bool', value=True)
+            ]
+            )
+
+        optsPen = boundPen or fn.mkPen()
+        for p in param:
+            name = p.name()
+            # Qt naming scheme uses isXXX for booleans
+            if isinstance(p.value(), bool):
+                attrName = f'is{name.title()}'
+            else:
+                attrName = name
+            default = getattr(optsPen, attrName)()
+            replace = r'\1 \2'
+            name = re.sub(r'(\w)([A-Z])', replace, name)
+            name = name.title().strip()
+            p.setOpts(title=name, default=default)
+
+        def setterWrapper(setter):
+            def newSetter(_, value):
+                setter(value)
+                self.sigValueChanged.emit(self, self.pen)
+
+            return newSetter
+
+        if boundPen is not None:
+            self.updateFromPen(param, boundPen)
+            for p in param:
+                setter, setName = self._setterForParam(p.name(), boundPen, returnName=True)
+                # Instead, set the parameter which will signal the old setter
+                setattr(boundPen, setName, p.setValue)
+                p.sigValueChanged.connect(setterWrapper(setter))
+                # Force children to emulate self's value instead of being part of a tree like normal
+                p.sigValueChanged.disconnect(p._emitValueChanged)
+                # Populate initial value
+        return param
+
+    @staticmethod
+    def _setterForParam(paramName, obj, returnName=False):
+        formatted = paramName[0].upper() + paramName[1:]
+        setter = getattr(obj, f'set{formatted}')
+        if returnName:
+            return setter, formatted
+        return setter
+
+    @staticmethod
+    def updateFromPen(param, pen):
+        """
+        Applies settings from a pen to either a Parameter or dict. The Parameter or dict must already
+        be populated with the relevant keys that can be found in `PenSelectorDialog.mkParam`.
+        """
+        stack = ExitStack()
+        if isinstance(param, Parameter):
+            names = param.names
+            # Block changes until all are finalized
+            stack.enter_context(param.treeChangeBlocker())
+        else:
+            names = param
+        for opt in names:
+            # Booleans have different naming convention
+            if isinstance(param[opt], bool):
+                attrName = f'is{opt.title()}'
+            else:
+                attrName = opt
+            param[opt] = getattr(pen, attrName)()
+        stack.close()
