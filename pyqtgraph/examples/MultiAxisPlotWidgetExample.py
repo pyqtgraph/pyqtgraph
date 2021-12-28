a92ebe803ed9f013d7f549c6dff2064768909d87
matteo porta <matteo.sid@hotmail.it>
2021-12-28 04:20:06 +0100

    better example

--- pyqtgraph/examples/MultiAxisPlotWidgetExample.py
+++ pyqtgraph/examples/MultiAxisPlotWidgetExample.py
@@ -1,10 +1,32 @@
 import numpy as np
 
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtWidgets
+from pyqtgraph.Qt.QtCore import Qt
+from pyqtgraph.Qt.QtGui import QBrush, QColor, QGradient, QLinearGradient, QPen
+from pyqtgraph.Qt.QtWidgets import QMainWindow
+
+
+def mkStripedPen(colors, segment_lenght=20):
+    gradient = QLinearGradient(0, 0, segment_lenght, 0)
+    gradient.setCoordinateMode(QGradient.LogicalMode)
+    gradient.setSpread(QGradient.RepeatSpread)
+    stops = []
+    previous = None
+    for i, color in enumerate(colors + [None]):
+        if previous is not None:
+            stops.append([i / len(colors) - 0.001, previous])
+        if color is not None:
+            stops.append([i / len(colors), color])
+        previous = color
+    gradient.setStops(stops)
+    brush = QBrush(gradient)
+    pen = QPen(brush, 2, Qt.SolidLine, Qt.SquareCap, Qt.BevelJoin)
+    pen.setCosmetic(True)
+    return pen
+
 
 app = pg.mkQApp()
-mw = QtWidgets.QMainWindow()
+mw = QMainWindow()
 mw.resize(800, 800)
 pg.setConfigOption("background", "w")
 pg.setConfigOption("foreground", "k")
@@ -20,26 +42,36 @@ mpw.addLegend(offset=(0, 0))
 mpw.setTitle("MultiAxisPlotWidget Example")
 # AXYS
 ax1 = mpw.addAxis("sx1", "bottom", text="Samples1", units="sx1")
+ax1c = QColor("red")
+ax1.setPen(ax1c)
 ax2 = mpw.addAxis("sx2", "bottom", text="Samples2", units="sx2")
-ax3 = mpw.addAxis("sx3", "bottom", text="Samples3", units="sx3")
+ax2c = QColor("green")
+ax2.setPen(ax2c)
 ay1 = mpw.addAxis("sy1", "left", text="Data1", units="sy1")
+ay1c = QColor("cyan")
+ay1.setPen(ay1c)
 ay2 = mpw.addAxis("sy2", "left", text="Data2", units="sy2")
-ay3 = mpw.addAxis("sy3", "left", text="Data3", units="sy3")
+ay2c = QColor("magenta")
+ay2.setPen(ay2c)
 # CHARTS
-c0, _ = mpw.addChart("Dataset 0")
+c0, pi0 = mpw.addChart("Dataset 0")
+c0.setPen("black")
 c1, pi1 = mpw.addChart("Dataset 1", xAxisName="sx1", yAxisName="sy1")
+c1.setPen(mkStripedPen([ax1c, ay1c]))
 c2, pi2 = mpw.addChart("Dataset 2", xAxisName="sx2", yAxisName="sy1")
+c2.setPen(mkStripedPen([ax2c, ay1c]))
 c3, pi3 = mpw.addChart("Dataset 3", xAxisName="sx2", yAxisName="sy2")
-c4, pi4 = mpw.addChart("Dataset 4", xAxisName="sx3", yAxisName="sy3")
-c5, pi5 = mpw.addChart("Dataset 5")  # will create it's axes automatically
-c6, pi6 = mpw.addChart("Dataset 6")  # will not be displayed (not in the makeLayout's chart list)
+c3.setPen(mkStripedPen([ax2c, ay2c]))
 # make and display chart
 mpw.makeLayout(
-    axes=["sx2", "sx3", "sx1", "sy2", "sy3", "sy1", ],  # optional, selects and orders axes displayed
-    charts=["Dataset 0", "Dataset 1", "Dataset 2", "Dataset 3", "Dataset 4", "Dataset 5"]  # optional, selects charts displayed
+    # optional, selects and orders axes displayed.
+    # remember to include the default axes if used.
+    axes=["bottom", "sx1", "sx2", "sy2", "sy1", "left"],
+    # optional, selects charts displayed
+    charts=["Dataset 0", "Dataset 1", "Dataset 2", "Dataset 3"]
 )
 
-for i, c in enumerate([c0, c1, c2, c3, c4, c5, c6, ]):
+for i, c in enumerate([c0, c1, c2, c3, ], start=1):
     c.setData(np.array(np.sin(np.linspace(0, i * np.pi, num=1000))))
 
 mpw.update()
