706cd9a7337e8d7b1e41504d5e83fd03d635627f
Dalton Caron <dcaron@mtech.edu>
2022-07-19 01:11:52 -0700

    haiiliin's excellent solution to the two constructor dilemma.

--- pyqtgraph/widgets/MatplotlibWidget.py
+++ pyqtgraph/widgets/MatplotlibWidget.py
@@ -3,34 +3,38 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 from matplotlib.figure import Figure
 
 from ..Qt import QtWidgets
+import typing
 
 __all__ = ['MatplotlibWidget']
 
 class MatplotlibWidget(QtWidgets.QWidget):
-    """
-    Implements a Matplotlib figure inside a QWidget.
-    Use getFigure() and redraw() to interact with matplotlib.
     
-    Example::
-    
-        mw = MatplotlibWidget()
-        subplot = mw.getFigure().add_subplot(111)
-        subplot.plot(x,y)
-        mw.draw()
-    """
-    
-    def __init__(self, parent=None, flags=None, size=(5.0, 4.0), dpi=100):
-        QtWidgets.QWidget.__init__(self, parent=parent, flags=flags)
-        self.fig = Figure(size, dpi=dpi)
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        
-        self.vbox = QtWidgets.QVBoxLayout()
-        self.vbox.addWidget(self.toolbar)
-        self.vbox.addWidget(self.canvas)
-        
-        self.setLayout(self.vbox)
+    @typing.overload
+    def __init__(self, figsize=(5.0, 4.0), dpi=100, parent=None, flags=None):
+        pass
+
+    @typing.overload
+    def __init__(self, parent=None, flags=None):
+        pass
+
+    def __init__(self, *args, **kwargs):
+        if (
+            (args and not isinstance(args[0], QtWidgets.QWidget))  # figsize is provided in the positional args
+            or
+            (kwargs and ("figsize" in kwargs or "dpi" in kwargs))  # figsize or dpi are provided in keyword args
+        ):  # If figsize or dpi are provided
+            figsize = args[0] if len(args) > 0 else kwargs.get("figsize", (5.0, 4.0))
+            dpi = args[1] if len(args) > 1 else kwargs.get("dpi", 100)
+            parent = args[2] if len(args) > 2 else kwargs.get("parent", None)
+            flags = args[3] if len(args) > 3 else kwargs.get("flags", None)
+        else:
+            figsize = (5.0, 4.0)
+            dpi = 100
+            parent = args[0] if len(args) > 0 else kwargs.get("parent", None)
+            flags = args[1] if len(args) > 1 else kwargs.get("flags", None)
+        super().__init__(parent, flags)
+        self.figsize = figsize
+        self.dpi = dpi
 
     def getFigure(self):
         return self.fig
