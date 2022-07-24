60fb9783643e2eee8674f61a43226e093bcb576c
Jon <jonmatthis@gmai.com>
2022-07-24 12:19:09 -0400

    dare I engage in such hubris as to list the `jupyter_console_example.py` example as `recommended=True`?

--- pyqtgraph/examples/utils.py
+++ pyqtgraph/examples/utils.py
@@ -15,7 +15,7 @@ examples_ = OrderedDict([
     ('Matrix Display', 'MatrixDisplayExample.py'),
     ('ViewBox Features', Namespace(filename='ViewBoxFeatures.py', recommended=True)),
     ('Dock widgets', 'dockarea.py'),
-    ('Rich Jupyter Console', 'jupyter_console_example.py'),
+    ('Rich Jupyter Console',  Namespace(filename='jupyter_console_example.py', recommended=True)),
     ('Console', 'ConsoleWidget.py'),
     ('Histograms', 'histogram.py'),
     ('Beeswarm plot', 'beeswarm.py'),
