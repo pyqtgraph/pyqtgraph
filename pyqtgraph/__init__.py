5d3941e0f9c53921f2675cb05fd7724a7518764f
Dalton Caron <dpcaron99@gmail.com>
2022-08-06 21:27:51 -0600

    Modify MatplotlibWidget to accept QWidget super constructor parameters. (#2366)
    * Modify MatplotlibWidget to accept QWidget super constructor parameters.
    
    Co-authored-by: Dalton Caron <dcaron@mtech.edu>

--- pyqtgraph/__init__.py
+++ pyqtgraph/__init__.py
@@ -9,6 +9,7 @@ __version__ = '0.12.4.dev0'
 
 import os
 import sys
+import importlib
 
 import numpy  # # pyqtgraph requires numpy
 
@@ -287,6 +288,10 @@ from .widgets.TableWidget import *
 from .widgets.TreeWidget import *
 from .widgets.ValueLabel import *
 from .widgets.VerticalLabel import *
+
+# Wrapped to prevent matplotlib becoming a hard dependency.
+if spec := importlib.util.find_spec('matplotlib') is not None:
+    from .widgets.MatplotlibWidget import *
 
 ##############################################################
 ## PyQt and PySide both are prone to crashing on exit. 
