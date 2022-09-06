fe099a825f14bce0f5534d5f995453b2094502de
Martin Chase <outofculture@gmail.com>
2022-09-06 13:56:08 -0700

    a linspace across 31 points gives the values I expect

--- tests/imageview/test_imageview.py
+++ tests/imageview/test_imageview.py
@@ -15,19 +15,18 @@ def test_nan_image():
 
 
 def test_timeslide_snap():
-    count = 30
+    count = 31
     frames = np.ones((count, 10, 10))
     iv = pg.ImageView(discreteTimeLine=True)
     assert iv.nframes == 0
-    iv.setImage(frames, xvals=np.linspace(0., 1., count))
+    iv.setImage(frames, xvals=(np.linspace(0., 1., count)))
     iv.show()
     assert iv.nframes == count
     iv.play(count / 2)
-    iv.timeLine.setPos(0.49999)
+    iv.timeLine.setPos(0.51)
     ind, val = iv.timeIndex(iv.timeLine)
-    # :TODO: this val isn't what I expect, nor did I expect the `- 1` in the next statement.
-    assert ind == (count / 2) - 1
-    # assert val == 0.5
+    assert ind == count // 2
+    assert val == 0.5
 
 
 def test_init_with_mode_and_imageitem():
