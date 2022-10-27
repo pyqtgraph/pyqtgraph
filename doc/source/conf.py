0cf9b352106ec226b8cdc088f03c90c3c1cf18a9
Ognyan Moore <ognyan.moore@gmail.com>
2022-10-26 20:16:33 -0700

    Update copyright to pyqtgraph developers

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -80,7 +80,7 @@ project = 'pyqtgraph'
 now = datetime.utcfromtimestamp(
     int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
 )
-copyright = '2011 - {}, Luke Campagnola'.format(now.year)
+copyright = '2011 - {}, PyQtGraph developers'.format(now.year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
