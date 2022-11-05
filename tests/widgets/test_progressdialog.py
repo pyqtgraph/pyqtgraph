d94467fd0fed523370cd2725d0ff706c284295eb
KIU Shueng Chuan <nixchuan@gmail.com>
2022-11-05 10:08:02 +0800

    skip progressdialog on newer PySide6 and linux

--- tests/widgets/test_progressdialog.py
+++ tests/widgets/test_progressdialog.py
@@ -1,8 +1,16 @@
-from pyqtgraph import ProgressDialog, mkQApp
+import pytest
+import sys
 
-mkQApp()
+import pyqtgraph as pg
 
+pg.mkQApp()
 
+@pytest.mark.skipif(
+    sys.platform.startswith("linux")
+    and pg.Qt.QT_LIB == "PySide6"
+    and pg.Qt.PySide6.__version_info__ > (6, 3),
+    reason="taking gui thread causes segfault"
+)
 def test_progress_dialog():
-    with ProgressDialog("test", 0, 1) as dlg:
+    with pg.ProgressDialog("test", 0, 1) as dlg:
         dlg += 1
