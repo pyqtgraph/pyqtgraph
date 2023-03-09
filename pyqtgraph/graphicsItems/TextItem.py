10d4805ead0d61285d8dbf677fab1bfcdba7c72b
redruin1 <tfschaefer222@gmail.com>
2023-03-09 10:25:56 -0500

    Add `dataBounds()` method to `TextItem`
    Sacrifices some visual polish to make the autorange functionality with TextItems much more predictable.

--- pyqtgraph/graphicsItems/TextItem.py
+++ pyqtgraph/graphicsItems/TextItem.py
@@ -150,6 +150,18 @@ class TextItem(GraphicsObject):
         offset = (br - tl) * self.anchor
         self.textItem.setPos(-offset)
 
+    def dataBounds(self, ax, frac=1.0, orthoRange=None):
+        """
+        Returns only the anchor point for when calulating view ranges.
+        
+        Sacrifices some visual polish for fixing issue #2642.
+        """
+        if orthoRange:
+            range_min, range_max = orthoRange[0], orthoRange[1]
+            if not range_min <= self.anchor[ax] <= range_max:
+                return [None, None]
+
+        return [self.anchor[ax], self.anchor[ax]]
         
     def boundingRect(self):
         return self.textItem.mapRectToParent(self.textItem.boundingRect())
