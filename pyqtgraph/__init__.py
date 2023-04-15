4f642c9bf1abb3e66d89dbf5ef441e49a5ededf1
ntjess <ntjessu@gmail.com>
2023-04-14 23:45:57 -0500

    bump to dev version (#2693)

--- pyqtgraph/__init__.py
+++ pyqtgraph/__init__.py
@@ -3,7 +3,7 @@ PyQtGraph - Scientific Graphics and GUI Library for Python
 www.pyqtgraph.org
 """
 
-__version__ = '0.13.3'
+__version__ = '0.13.4.dev0'
 
 ### import all the goodies and add some helper functions for easy CLI use
 
