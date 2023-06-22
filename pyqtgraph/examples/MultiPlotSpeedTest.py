6f0696ff41d7d75d7de9465a85dd39c82abd47dd
KIU Shueng Chuan <nixchuan@gmail.com>
2023-06-22 23:18:30 +0800

    MultiPlotSpeedTest: count frames

--- pyqtgraph/examples/MultiPlotSpeedTest.py
+++ pyqtgraph/examples/MultiPlotSpeedTest.py
@@ -3,12 +3,11 @@
 Test the speed of rapidly updating multiple plot curves
 """
 
-from time import perf_counter
-
 import numpy as np
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
+from utils import FrameCounter
 
 # pg.setConfigOptions(useOpenGL=True)
 app = pg.mkQApp("MultiPlot Speed Test")
@@ -36,30 +35,21 @@ plot.addItem(rgn)
 
 data = np.random.normal(size=(nPlots*23,nSamples))
 ptr = 0
-lastTime = perf_counter()
-fps = None
-count = 0
 def update():
-    global curve, data, ptr, plot, lastTime, fps, nPlots, count
-    count += 1
+    global ptr
 
     for i in range(nPlots):
         curves[i].setData(data[(ptr+i)%data.shape[0]])
 
     ptr += nPlots
-    now = perf_counter()
-    dt = now - lastTime
-    lastTime = now
-    if fps is None:
-        fps = 1.0/dt
-    else:
-        s = np.clip(dt*3., 0, 1)
-        fps = fps * (1-s) + (1.0/dt) * s
-    plot.setTitle('%0.2f fps' % fps)
-    #app.processEvents()  ## force complete redraw for every plot
+    framecnt.update()
+
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(0)
+
+framecnt = FrameCounter()
+framecnt.sigFpsUpdate.connect(lambda fps: plot.setTitle(f'{fps:.1f} fps'))
 
 if __name__ == '__main__':
     pg.exec()
