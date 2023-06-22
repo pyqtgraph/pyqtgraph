43b188badbe2b5e8126eea0e63070c4a48c65f1b
KIU Shueng Chuan <nixchuan@gmail.com>
2023-06-22 22:42:03 +0800

    create FrameCounter class

--- pyqtgraph/examples/utils.py
+++ pyqtgraph/examples/utils.py
@@ -1,5 +1,7 @@
 from argparse import Namespace
 from collections import OrderedDict
+from time import perf_counter
+from pyqtgraph.Qt import QtCore
 
 # Avoid clash with module name
 examples_ = OrderedDict([
@@ -135,3 +137,28 @@ trivial = dict([
 skiptest = dict([
     ('ProgressDialog', 'ProgressDialog.py'),    # modal dialog
 ])
+
+
+class FrameCounter(QtCore.QObject):
+    sigFpsUpdate = QtCore.Signal(object)
+
+    def __init__(self, interval=1000):
+        super().__init__()
+        self.count = 0
+        self.last_update = 0
+        self.interval = interval
+
+    def update(self):
+        self.count += 1
+
+        if self.last_update == 0:
+            self.last_update = perf_counter()
+            self.startTimer(self.interval)
+
+    def timerEvent(self, evt):
+        now = perf_counter()
+        elapsed = now - self.last_update
+        fps = self.count / elapsed
+        self.last_update = now
+        self.count = 0
+        self.sigFpsUpdate.emit(fps)
