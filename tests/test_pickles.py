b5083cf6047466a55baf0b5b91bef11f154bbfe2
Martin Chase <outofculture@gmail.com>
2024-01-17 17:02:09 -0800

    test runners are really very capable

--- tests/test_pickles.py
+++ tests/test_pickles.py
@@ -14,8 +14,3 @@ def test_SRTTransform3D():
     a = SRTTransform({'scale': 2, 'angle': math.pi / 2})
     b = pickle.loads(pickle.dumps(a))
     assert a == b
-
-
-if __name__ == '__main__':
-    test_SRTTransform()
-    test_SRTTransform3D()
