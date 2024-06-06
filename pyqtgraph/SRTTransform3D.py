418dbfef5eec65c34652bd7a7a60cd1bcd8ce90c
Martin Chase <outofculture@gmail.com>
2024-06-06 11:40:44 -0700

    cleanup: remove method definded identically to parent class

--- pyqtgraph/SRTTransform3D.py
+++ pyqtgraph/SRTTransform3D.py
@@ -214,17 +214,6 @@ class SRTTransform3D(Transform3D):
 
     def __repr__(self):
         return str(self.saveState())
-        
-    def matrix(self, nd=3):
-        if nd == 3:
-            return np.array(self.copyDataTo()).reshape(4,4)
-        elif nd == 2:
-            m = np.array(self.copyDataTo()).reshape(4,4)
-            m[2] = m[3]
-            m[:,2] = m[:,3]
-            return m[:3,:3]
-        else:
-            raise Exception("Argument 'nd' must be 2 or 3")
 
     def __reduce__(self):
         return SRTTransform3D, (self.saveState(),)
