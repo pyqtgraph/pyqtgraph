967c7a8b2e78ea22ba75e54fb29056e255b4136a
PauloGitHB <paulleva82@gmail.com>
2024-12-18 14:09:14 +0100

    parametertree.py

--- pyqtgraph/examples/parametertree.py
+++ pyqtgraph/examples/parametertree.py
@@ -18,6 +18,8 @@ app = pg.mkQApp("Parameter Tree Example")
 import pyqtgraph.parametertree.parameterTypes as pTypes
 from pyqtgraph.parametertree import Parameter, ParameterTree
 
+from xml.etree import ElementTree as ET
+from parametertree.xml_factory import XMLParameter, XMLParameterFactory
 
 ## test subclassing parameters
 ## This parameter automatically generates two child parameters which are always reciprocals of each other
@@ -106,7 +108,6 @@ def change(param, changes):
     
 p.sigTreeStateChanged.connect(change)
 
-
 def valueChanging(param, value):
     print("Value changing (not finalized): %s %s" % (param, value))
     
@@ -141,6 +142,45 @@ win.setLayout(layout)
 layout.addWidget(QtWidgets.QLabel("These are two views of the same data. They should always display the same values."), 0,  0, 1, 2)
 layout.addWidget(t, 1, 0, 1, 1)
 layout.addWidget(t2, 1, 1, 1, 1)
+win.show()
+
+## test save/restore
+state = p.saveState()
+p.restoreState(state)
+compareState = p.saveState()
+assert pg.eq(compareState, state)
+
+def save_to_xml():
+    global xml_data
+    xml_data = XMLParameterFactory.parameter_to_xml_string_factory(p)
+    print("Export XML:\n", xml_data)
+
+def load_from_xml():
+    global xml_data
+    if xml_data:
+        param_dict = XMLParameterFactory.XML_string_to_parameter(xml_data)
+        restored_params = Parameter.create(name='Restored Parameters', type='group', children=param_dict)
+        t.setParameters(restored_params, showTop=False)
+        print("Import XML completed")
+
+btn_export = QtWidgets.QPushButton("Export to XML")
+btn_import = QtWidgets.QPushButton("Import from XML")
+btn_export.clicked.connect(save_to_xml)
+btn_import.clicked.connect(load_from_xml)
+
+
+## Create two ParameterTree widgets, both accessing the same data
+t = ParameterTree()
+t.setParameters(p, showTop=False)
+t.setWindowTitle('pyqtgraph example: Parameter Tree')
+
+win = QtWidgets.QWidget()
+layout = QtWidgets.QGridLayout()
+win.setLayout(layout)
+layout.addWidget(QtWidgets.QLabel("These are two views of the same data. They should always display the same values."), 0,  0, 1, 2)
+layout.addWidget(t, 1, 0, 1, 2)
+layout.addWidget(btn_export, 2, 0)
+layout.addWidget(btn_import, 2, 1)
 win.show()
 
 ## test save/restore
