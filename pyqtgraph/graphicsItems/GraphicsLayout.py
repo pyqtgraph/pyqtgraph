pyqtgraph/pyqtgraph

commit 54efcbd2398c0f39a11ec1d14fe8f7f3f3a6b901

Update removeItem()
As addressed in issue #3085
After removing the item, explicitly adjusted the row and column stretches to ensure the layout resizes correctly.

pyqtgraph/graphicsItems/GraphicsLayout.py
@@ -174,18 +174,31 @@ class GraphicsLayout(GraphicsWidget):
     def removeItem(self, item):
         """Remove *item* from the layout."""
         ind = self.itemIndex(item)
+        
+        # Remove the item from the layout and scene
         self.layout.removeAt(ind)
         self.scene().removeItem(item)
         
+        # Clear the row and column where the item was
         for r, c in self.items[item]:
             del self.rows[r][c]
+            
+            # Adjust the layout by updating row/column stretch factors
+            self.layout.setRowStretchFactor(r, 0)
+            self.layout.setColumnStretchFactor(c, 0)
+        
+        # Clean up the references to the removed item
         del self.items[item]
-
         item.geometryChanged.disconnect(self._updateItemBorder)
+        
+        # Remove the item's border
         itemBorder = self.itemBorders.pop(item)
         self.scene().removeItem(itemBorder)
-
+        
+        # Recalculate the layout to reclaim the space
+        self.layout.updateGeometry()
         self.update()
+
     
     def clear(self):
         """Remove all items from the layout and set the current row and column to 0

commit fd353ded1b6660e08d05cad44ae3541c9e1ec842

GraphicsLayout: Fix removeItem() regression
This fixes a regression introduced in
54efcbd2398c0f39a11ec1d14fe8f7f3f3a6b901, where the stretch factors for
the row/column previously containing a removed item were zeroed. In
general, this is of course incorrect, as there will be other items
sharing the same row/column that should not be disturbed. The intention
behind that part of the change is not clear to me; this commit simply
reverts it.

pyqtgraph/graphicsItems/GraphicsLayout.py
@@ -182,10 +182,6 @@ class GraphicsLayout(GraphicsWidget):
         # Clear the row and column where the item was
         for r, c in self.items[item]:
             del self.rows[r][c]
-            
-            # Adjust the layout by updating row/column stretch factors
-            self.layout.setRowStretchFactor(r, 0)
-            self.layout.setColumnStretchFactor(c, 0)
         
         # Clean up the references to the removed item
         del self.items[item]