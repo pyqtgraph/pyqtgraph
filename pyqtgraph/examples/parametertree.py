c22dc029bb5fe4335c5eba3c90678b0cd575a020
Ashwola <schouderconstant@outlook.com>
2025-09-17 18:17:06 +0200

    Expanding examples in parametertree

--- pyqtgraph/examples/parametertree.py
+++ pyqtgraph/examples/parametertree.py
@@ -23,17 +23,33 @@ from pyqtgraph.parametertree import Parameter, ParameterTree
 ## This parameter automatically generates two child parameters which are always reciprocals of each other
 class ComplexParameter(pTypes.GroupParameter):
     def __init__(self, **opts):
-        opts['type'] = 'bool'
-        opts['value'] = True
+        opts["type"] = "bool"
+        opts["value"] = True
         pTypes.GroupParameter.__init__(self, **opts)
-        
-        self.addChild({'name': 'A = 1/B', 'type': 'float', 'value': 7, 'suffix': 'Hz', 'siPrefix': True})
-        self.addChild({'name': 'B = 1/A', 'type': 'float', 'value': 1/7., 'suffix': 's', 'siPrefix': True})
-        self.a = self.param('A = 1/B')
-        self.b = self.param('B = 1/A')
+
+        self.addChild(
+            {
+                "name": "A = 1/B",
+                "type": "float",
+                "value": 7,
+                "suffix": "Hz",
+                "siPrefix": True,
+            }
+        )
+        self.addChild(
+            {
+                "name": "B = 1/A",
+                "type": "float",
+                "value": 1 / 7.0,
+                "suffix": "s",
+                "siPrefix": True,
+            }
+        )
+        self.a = self.param("A = 1/B")
+        self.b = self.param("B = 1/A")
         self.a.sigValueChanged.connect(self.aChanged)
         self.b.sigValueChanged.connect(self.bChanged)
-        
+
     def aChanged(self):
         self.b.setValue(1.0 / self.a.value(), blockSignal=self.bChanged)
 
@@ -45,50 +61,124 @@ class ComplexParameter(pTypes.GroupParameter):
 ## this group includes a menu allowing the user to add new parameters into its child list
 class ScalableGroup(pTypes.GroupParameter):
     def __init__(self, **opts):
-        opts['type'] = 'group'
-        opts['addText'] = "Add"
-        opts['addList'] = ['str', 'float', 'int']
+        opts["type"] = "group"
+        opts["addText"] = "Add"
+        # opts['addList'] = ['str', 'float', 'int']
+        # opts['addMenu'] = ['str', 'float', 'int']
+        addMenu = [
+            {
+                "Basic Types": ["int", "float", "str", "bool"],
+                "Collections": [
+                    "list",
+                    "checklist",
+                ],
+                "Widgets": {
+                    "Base": ["slider", "action"],
+                    "Special": ["pen", "calendar"],
+                    "Extras": ["font", "colormap", "cmaplut", "color", "file"],
+                },
+            },
+        ]
+        opts["addMenu"] = addMenu
         pTypes.GroupParameter.__init__(self, **opts)
-    
+
     def addNew(self, typ):
+        if isinstance(typ, tuple):
+            typ = typ[-1]  # Get the last element from the path tuple
+
         val = {
-            'str': '',
-            'float': 0.0,
-            'int': 0
-        }[typ]
-        self.addChild(dict(name="ScalableParam %d" % (len(self.childs)+1), type=typ, value=val, removable=True, renamable=True))
+            # Basic types
+            "str": "",
+            "float": 0.0,
+            "int": 0,
+            # Collections
+            "list": {"value": 50, "default": 50, "limits": [0, 50, 100]},
+            "checklist": {"value": 50, "default": 50, "limits": [0, 50, 100]},
+            # Widget types
+            "slider": {"value": 50, "limits": [0, 100]},
+            "checkbox": False,
+            "bool": False,
+            "font": "Inter",
+            "cmaplut": "viridis",
+            "color": "#fff",
+        }.get(typ, None)  # Default to empty string if type not found
 
+        # Handle special cases where we need to create different parameter structures
+        if typ in ["list", "checklist", "slider"]:
+            param_dict = dict(
+                name="ScalableParam %d" % (len(self.childs) + 1),
+                type=typ,
+                value=val["value"],
+                limits=val["limits"],
+                removable=True,
+                renamable=True,
+            )
 
+        else:
+            # Standard parameter creation
+            param_dict = dict(
+                name="ScalableParam %d" % (len(self.childs) + 1),
+                type=typ,
+                value=val,
+                removable=True,
+                renamable=True,
+            )
+
+        self.addChild(param_dict)
 
 
 params = [
     makeAllParamTypes(),
-    {'name': 'Save/Restore functionality', 'type': 'group', 'children': [
-        {'name': 'Save State', 'type': 'action'},
-        {'name': 'Restore State', 'type': 'action', 'children': [
-            {'name': 'Add missing items', 'type': 'bool', 'value': True},
-            {'name': 'Remove extra items', 'type': 'bool', 'value': True},
-        ]},
-    ]},
-    {'name': 'Custom context menu', 'type': 'group', 'children': [
-        {'name': 'List contextMenu', 'type': 'float', 'value': 0, 'context': [
-            'menu1',
-            'menu2'
-        ]},
-        {'name': 'Dict contextMenu', 'type': 'float', 'value': 0, 'context': {
-            'changeName': 'Title',
-            'internal': 'What the user sees',
-        }},
-    ]},
-    ComplexParameter(name='Custom parameter group (reciprocal values)'),
-    ScalableGroup(name="Expandable Parameter Group", tip='Click to add children', children=[
-        {'name': 'ScalableParam 1', 'type': 'str', 'value': "default param 1"},
-        {'name': 'ScalableParam 2', 'type': 'str', 'value': "default param 2"},
-    ]),
+    {
+        "name": "Save/Restore functionality",
+        "type": "group",
+        "children": [
+            {"name": "Save State", "type": "action"},
+            {
+                "name": "Restore State",
+                "type": "action",
+                "children": [
+                    {"name": "Add missing items", "type": "bool", "value": True},
+                    {"name": "Remove extra items", "type": "bool", "value": True},
+                ],
+            },
+        ],
+    },
+    {
+        "name": "Custom context menu",
+        "type": "group",
+        "children": [
+            {
+                "name": "List contextMenu",
+                "type": "float",
+                "value": 0,
+                "context": ["menu1", "menu2"],
+            },
+            {
+                "name": "Dict contextMenu",
+                "type": "float",
+                "value": 0,
+                "context": {
+                    "changeName": "Title",
+                    "internal": "What the user sees",
+                },
+            },
+        ],
+    },
+    ComplexParameter(name="Custom parameter group (reciprocal values)"),
+    ScalableGroup(
+        name="Expandable Parameter Group",
+        tip="Click to add children",
+        children=[
+            {"name": "ScalableParam 1", "type": "str", "value": "default param 1"},
+            {"name": "ScalableParam 2", "type": "str", "value": "default param 2"},
+        ],
+    ),
 ]
 
 ## Create tree of Parameter objects
-p = Parameter.create(name='params', type='group', children=params)
+p = Parameter.create(name="params", type="group", children=params)
+
 
 ## If anything changes in the tree, print a message
 def change(param, changes):
@@ -96,49 +186,63 @@ def change(param, changes):
     for param, change, data in changes:
         path = p.childPath(param)
         if path is not None:
-            childName = '.'.join(path)
+            childName = ".".join(path)
         else:
             childName = param.name()
-        print('  parameter: %s'% childName)
-        print('  change:    %s'% change)
-        print('  data:      %s'% str(data))
-        print('  ----------')
-    
+        print("  parameter: %s" % childName)
+        print("  change:    %s" % change)
+        print("  data:      %s" % str(data))
+        print("  ----------")
+
+
 p.sigTreeStateChanged.connect(change)
 
 
 def valueChanging(param, value):
     print("Value changing (not finalized): %s %s" % (param, value))
-    
+
+
 # Only listen for changes of the 'widget' child:
-for child in p.child('Example Parameters'):
-    if 'widget' in child.names:
-        child.child('widget').sigValueChanging.connect(valueChanging)
+for child in p.child("Example Parameters"):
+    if "widget" in child.names:
+        child.child("widget").sigValueChanging.connect(valueChanging)
+
 
 def save():
     global state
     state = p.saveState()
 
+
 def restore():
     global state
-    add = p['Save/Restore functionality', 'Restore State', 'Add missing items']
-    rem = p['Save/Restore functionality', 'Restore State', 'Remove extra items']
+    add = p["Save/Restore functionality", "Restore State", "Add missing items"]
+    rem = p["Save/Restore functionality", "Restore State", "Remove extra items"]
     p.restoreState(state, addChildren=add, removeChildren=rem)
-p.param('Save/Restore functionality', 'Save State').sigActivated.connect(save)
-p.param('Save/Restore functionality', 'Restore State').sigActivated.connect(restore)
+
+
+p.param("Save/Restore functionality", "Save State").sigActivated.connect(save)
+p.param("Save/Restore functionality", "Restore State").sigActivated.connect(restore)
 
 
 ## Create two ParameterTree widgets, both accessing the same data
 t = ParameterTree()
 t.setParameters(p, showTop=False)
-t.setWindowTitle('pyqtgraph example: Parameter Tree')
+t.setWindowTitle("pyqtgraph example: Parameter Tree")
 t2 = ParameterTree()
 t2.setParameters(p, showTop=False)
 
 win = QtWidgets.QWidget()
 layout = QtWidgets.QGridLayout()
 win.setLayout(layout)
-layout.addWidget(QtWidgets.QLabel("These are two views of the same data. They should always display the same values."), 0,  0, 1, 2)
+layout.addWidget(
+    QtWidgets.QLabel(
+        "These are two views of the same data. They should always display the same values."
+    ),
+    0,
+    0,
+    1,
+    2,
+)
 layout.addWidget(t, 1, 0, 1, 1)
 layout.addWidget(t2, 1, 1, 1, 1)
 win.show()
@@ -149,5 +253,5 @@ p.restoreState(state)
 compareState = p.saveState()
 assert pg.eq(compareState, state)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pg.exec()
