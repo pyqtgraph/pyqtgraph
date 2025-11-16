a341be189f209a4f839459b3076e1d19c693bfad
Ognyan Moore <ognyan.moore@gmail.com>
2025-11-16 22:32:03 +0300

    Release 0.14.0

--- pyqtgraph/__init__.py
+++ pyqtgraph/__init__.py
@@ -3,7 +3,7 @@ PyQtGraph - Scientific Graphics and GUI Library for Python
 www.pyqtgraph.org
 """
 
-__version__ = '0.14.0dev0'
+__version__ = '0.14.0'
 
 ### import all the goodies and add some helper functions for easy CLI use
 
