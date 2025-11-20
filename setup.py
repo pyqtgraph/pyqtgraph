c5fe4a24eccef25b150a211516ea8786cd3eb13a
Ogi Moore <ognyan.moore@gmail.com>
2025-11-20 22:17:33 +0300

    Bump up dependencies to conform to SPEC-0 (#3418)
    * Bump up dependencies to conform to SPEC-0
    * Bump min Qt6 version to 6.8
    * Bump sphinx to 8.2.3 and fix some documentation warnings
    * Add twine check step to verify the wheel and expand on package metadata displayed in PyPi
    * Remove single file binary code generation (untested for years)
    * Upgrade pre-commit-config
    * Bump mesa drivers used on windows CI testing for opengl

--- setup.py
+++ setup.py
@@ -1,111 +1,51 @@
-DESCRIPTION = """\
-PyQtGraph is a pure-python graphics and GUI library built on PyQt5/PySide2 and
-numpy. 
+import os
+import sys
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
 
-It is intended for use in mathematics / scientific / engineering applications.
-Despite being written entirely in python, the library is very fast due to its
-heavy leverage of numpy for number crunching, Qt's GraphicsView framework for
-2D display, and OpenGL for 3D display.
-"""
+root_path = Path(__file__).parent
+sys.path.append(os.fsdecode(root_path))
+import tools.setupHelpers as helpers
 
-setupOpts = dict(
+version = helpers.getInitVersion(root_path / "pyqtgraph")
+long_description = (root_path / "README.md").read_text()
+
+setup(
     name='pyqtgraph',
     description='Scientific Graphics and GUI Library for Python',
-    long_description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license =  'MIT',
     url='http://www.pyqtgraph.org',
     project_urls={
+        'Changelog': 'https://github.com/pyqtgraph/pyqtgraph/blob/master/CHANGELOG',
         'Documentation': 'https://pyqtgraph.readthedocs.io',
+        'Homepage': 'https://pyqtgraph.org',
+        'Mastodon': 'https://fosstodon.org/@pyqtgraph',
         'Source': 'https://github.com/pyqtgraph/pyqtgraph',
     },
     author='Luke Campagnola',
     author_email='luke.campagnola@gmail.com',
     classifiers = [
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
         "Environment :: Other Environment",
         "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Image Processing",
         "Topic :: Scientific/Engineering :: Visualization",
-        "Topic :: Software Development :: User Interfaces",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: User Interfaces"
     ],
-)
-
-
-import os
-import re
-import sys
-
-from setuptools import find_namespace_packages, setup
-from setuptools.command import install
-
-path = os.path.split(__file__)[0]
-sys.path.append(path)
-import tools.setupHelpers as helpers
-
-## Decide what version string to use in the build
-version, forcedVersion, gitVersion, initVersion = helpers.getVersionStrings(pkg='pyqtgraph')
-
-
-
-class Install(install.install):
-    """
-    * Check for previously-installed version before installing
-    * Set version string in __init__ after building. This helps to ensure that we
-      know when an installation came from a non-release code base.
-    """
-    def run(self):
-        global path, version, initVersion, forcedVersion, installVersion
-
-        name = self.config_vars['dist_name']
-        path = os.path.join(self.install_libbase, 'pyqtgraph')
-        if os.path.exists(path):
-            raise Exception("It appears another version of %s is already "
-                            "installed at %s; remove this before installing." 
-                            % (name, path))
-        print("Installing to %s" % path)
-        rval = install.install.run(self)
-
-        
-        # If the version in __init__ is different from the automatically-generated
-        # version string, then we will update __init__ in the install directory
-        if initVersion == version:
-            return rval
-        
-        try:
-            initfile = os.path.join(path, '__init__.py')
-            with open(initfile, "r") as file_:
-                data = file_.read()
-            with open(initfile, "w") as file_:
-                file_.write(re.sub(r"__version__ = .*", "__version__ = '%s'" % version, data))
-            installVersion = version
-        except:
-            sys.stderr.write("Warning: Error occurred while setting version string in build path. "
-                             "Installation will use the original version string "
-                             "%s instead.\n" % (initVersion)
-                             )
-            if forcedVersion:
-                raise
-            installVersion = initVersion
-            sys.excepthook(*sys.exc_info())
-    
-        return rval
-
-setup(
     version=version,
     cmdclass={
-        'install': Install,
-        'deb': helpers.DebCommand, 
-        'test': helpers.TestCommand,
-        'debug': helpers.DebugCommand,
-        'mergetest': helpers.MergeTestCommand,
         'asv_config': helpers.ASVConfigCommand,
-        'style': helpers.StyleCommand
     },
     packages=find_namespace_packages(include=['pyqtgraph', 'pyqtgraph.*']),
-    python_requires=">=3.10",
+    python_requires=">=3.12",
     package_dir={"pyqtgraph": "pyqtgraph"},
     package_data={
         'pyqtgraph.examples': ['optics/*.gz', 'relativity/presets/*.cfg'],
@@ -117,8 +57,7 @@ setup(
         ],
     },
     install_requires = [
-        'numpy>=1.25.0',
+        'numpy>=2.0.0',
         'colorama'
     ],
-    **setupOpts
 )
