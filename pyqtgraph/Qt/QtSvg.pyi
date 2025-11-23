7856b1952e253f359753db2ff464faafee46065e
KIU Shueng Chuan <nixchuan@gmail.com>
2025-11-23 11:40:21 +0800

    remove PySide2 from pyi files

--- pyqtgraph/Qt/QtSvg.pyi
+++ pyqtgraph/Qt/QtSvg.pyi
@@ -1,5 +1,4 @@
 # Import packages in reverse libOrder as defined in pyqtgraph/Qt/__init__.py
-from PySide2.QtSvg import *
 from PyQt5.QtSvg import *
 from PySide6.QtSvg import *
 from PyQt6.QtSvg import *
--- pyqtgraph/Qt/QtTest.pyi
+++ pyqtgraph/Qt/QtTest.pyi
@@ -1,5 +1,4 @@
 # Import packages in reverse libOrder as defined in pyqtgraph/Qt/__init__.py
-from PySide2.QtTest import *
 from PyQt5.QtTest import *
 from PySide6.QtTest import *
 from PyQt6.QtTest import *
