d92088e56f4ed3ecf4f3bea3d73f5b1c4e3539c7
KIU Shueng Chuan <nixchuan@gmail.com>
2025-11-24 22:42:53 +0800

    remove deprecated ActionGroup

--- pyqtgraph/parametertree/parameterTypes/actiongroup.py
+++ pyqtgraph/parametertree/parameterTypes/actiongroup.py
@@ -1,5 +1,3 @@
-import warnings
-
 from ...Qt import QtCore
 from .action import ParameterControlledButton
 from .basetypes import GroupParameter, GroupParameterItem
@@ -60,20 +58,3 @@ class ActionGroupParameter(GroupParameter):
         buttonOpts = self.opts.get("button", {}).copy()
         buttonOpts.update(opts)
         self.setOpts(button=buttonOpts)
-
-
-class ActionGroup(ActionGroupParameter):
-    sigActivated = QtCore.Signal()
-
-    def __init__(self, **opts):
-        warnings.warn(
-            "`ActionGroup` is deprecated and will be removed in the first release after "
-            "January 2023. Use `ActionGroupParameter` instead. See "
-            "https://github.com/pyqtgraph/pyqtgraph/pull/2505 for details.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        super().__init__(**opts)
-
-    def activate(self):
-        self.sigActivated.emit()
