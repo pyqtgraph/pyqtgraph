f465fb678e48952df891258561f320ccce7e4b68
Ashwola <schouderconstant@outlook.com>
2026-06-25 21:52:26 +0200

    ordering and adding back delete

--- pyqtgraph/icons/__init__.py
+++ pyqtgraph/icons/__init__.py
@@ -64,17 +64,17 @@ def getGraphPixmap(name, size=(20, 20)):
 # so these are intentionally not bound to a name (avoids CodeQL "unused
 # global variable" warnings); 
 # Look them up via getGraphIcon(name) or getGraphPixmap
+GraphIcon("auto.png")
+GraphIcon("ctrl.png")
+GraphIcon("default.png")
+GraphIcon("delete.png")
+GraphIcon("invisibleEye.svg")
+GraphIcon("lock.png")
 GraphIcon("rename.png")
 GraphIcon("revert_default.png")
 GraphIcon("set_default.png")
 GraphIcon("unlock.png")
 GraphIcon("visibleEye.svg")
-GraphIcon("auto.png")
-GraphIcon("ctrl.png")
-GraphIcon("default.png")
-GraphIcon("invisibleEye.svg")
-GraphIcon("lock.png")
-
 
 def __getattr__(name):
     # Deprecated: icons used to be exposed as module-level GraphIcon
