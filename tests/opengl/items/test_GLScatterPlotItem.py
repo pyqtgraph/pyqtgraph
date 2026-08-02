e9dd9863f2f915f7705416086bf6f9da90a81619
Mergepath <mergepath@local>
2026-08-02 13:48:35 +0000

    fix: address #3328 - PySide/QT version 6.9.1 incompatiblity
    Closes #3328

--- tests/opengl/items/test_GLScatterPlotItem.py
+++ tests/opengl/items/test_GLScatterPlotItem.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 pytest.importorskip('OpenGL')
 
@@ -11,3 +13,47 @@ def test_parentItem():
     parent = GLGraphicsItem()
     child = GLScatterPlotItem(parentItem=parent)
     ensure_parentItem(parent, child)
+
+
+def test_scatter_shader_program_disables_pyopengl_validation(monkeypatch):
+    scatter_module = sys.modules[GLScatterPlotItem.__module__]
+
+    class FakeFormat:
+        def version(self):
+            return (3, 1)
+
+    class FakeContext:
+        def format(self):
+            return FakeFormat()
+
+        def isOpenGLES(self):
+            return False
+
+    class FakeQOpenGLContext:
+        @staticmethod
+        def currentContext():
+            return FakeContext()
+
+    compile_program_calls = []
+
+    monkeypatch.setattr(GLScatterPlotItem, "_shaderProgram", None)
+    monkeypatch.setattr(scatter_module.QtGui, "QOpenGLContext", FakeQOpenGLContext)
+    monkeypatch.setattr(
+        scatter_module.shaders,
+        "compileShader",
+        lambda sources, shader_type: (sources, shader_type),
+    )
+
+    def compile_program(*compiled, validate=True):
+        compile_program_calls.append(validate)
+        return 1
+
+    monkeypatch.setattr(scatter_module.shaders, "compileProgram", compile_program)
+    monkeypatch.setattr(
+        scatter_module.GL, "glBindAttribLocation", lambda *args: None
+    )
+    monkeypatch.setattr(scatter_module.GL, "glLinkProgram", lambda *args: None)
+
+    GLScatterPlotItem.getShaderProgram()
+
+    assert compile_program_calls == [False]
