098f04b355c0ea6896eaf3c77209ed0596c331da
Anton Yablokov <stsav012@gmail.com>
2026-08-10 20:53:43 +0300

    Update tests for HDF5Exporter to reflect the changes in it

--- tests/exporters/test_hdf5.py
+++ tests/exporters/test_hdf5.py
@@ -4,8 +4,13 @@ from numpy.testing import assert_equal
 
 import pyqtgraph as pg
 from pyqtgraph.exporters import HDF5Exporter
+from pyqtgraph.Qt import QtCore
+
 
 h5py = pytest.importorskip("h5py")
+
+
+translate = QtCore.QCoreApplication.translate
 
 
 @pytest.fixture
@@ -15,33 +20,43 @@ def tmp_h5(tmp_path):
 
 @pytest.mark.parametrize("combine", [False, True])
 def test_HDF5Exporter(tmp_h5, combine):
-    # Basic test of functionality: multiple curves with shared x array. Tests
-    # both options for stacking the data (columnMode).
-    x = np.linspace(0, 1, 100)
-    y1 = np.sin(x)
-    y2 = np.cos(x)
+    # Test export with multiple curves of different size.
+    # Tests both options for stacking the data (columnMode).
+    x1 = np.linspace(0, 1, 10)
+    y1 = np.sin(x1)
+    x2 = np.linspace(0, 1, 100)
+    y2 = np.cos(x2)
 
     plt = pg.PlotWidget()
     plt.show()
-    plt.plot(x=x, y=y1)
-    plt.plot(x=x, y=y2)
+    plt.plot(x=x1, y=y1)
+    plt.plot(x=x2, y=y2)
 
     ex = HDF5Exporter(plt.plotItem)
 
-    if combine:
-        ex.parameters()['columnMode'] = '(x,y,y,y) for all plots'
+    if not combine:
+        ex.parameters()['columnMode'] = translate("Exporter", "separate x and y")
 
     ex.export(fileName=tmp_h5)
 
     with h5py.File(tmp_h5, 'r') as f:
-        # should be a single dataset with the name of the exporter
-        dset = f[ex.parameters()['Name']]
-        assert isinstance(dset, h5py.Dataset)
-
         if combine:
-            assert_equal(np.array([x, y1, y2]), dset)
+            # should be two datasets with default names
+            dset1 = f["Curve 1"]
+            assert isinstance(dset1, h5py.Dataset)
+            dset2 = f["Curve 2"]
+            assert isinstance(dset2, h5py.Dataset)
+            assert_equal(np.column_stack((x1, y1)), dset1)
+            assert_equal(np.column_stack((x2, y2)), dset2)
         else:
-            assert_equal(np.array([x, y1, x, y2]), dset)
+            # should be two groups with default names,
+            # two datasets with default names in each
+            assert isinstance(f["Curve 1"], h5py.Group)
+            assert_equal(x1, f["Curve 1"]["x"])
+            assert_equal(y1, f["Curve 1"]["y"])
+            assert isinstance(f["Curve 2"], h5py.Group)
+            assert_equal(x2, f["Curve 2"]["x"])
+            assert_equal(y2, f["Curve 2"]["y"])
 
 
 def test_HDF5Exporter_unequal_lengths(tmp_h5):
@@ -61,13 +76,16 @@ def test_HDF5Exporter_unequal_lengths(tmp_h5):
     ex.export(fileName=tmp_h5)
 
     with h5py.File(tmp_h5, 'r') as f:
-        # should be a group with the name of the exporter
-        group = f[ex.parameters()['Name']]
-        assert isinstance(group, h5py.Group)
+        # should be a group with the name of the curve
+        dset1 = f['plot0']
+        assert isinstance(dset1, h5py.Dataset)
+        # should be a group with the name by default
+        dset2 = f['Curve 2']
+        assert isinstance(dset2, h5py.Dataset)
 
         # should be a dataset under the group with the name of the PlotItem
-        assert_equal(np.array([x1, y1]), group['plot0'])
+        assert_equal(np.column_stack((x1, y1)), dset1)
 
         # should be a dataset under the group with a default name that's the
         # index of the curve in the PlotItem
-        assert_equal(np.array([x2, y2]), group['1'])
+        assert_equal(np.column_stack((x2, y2)), dset2)
