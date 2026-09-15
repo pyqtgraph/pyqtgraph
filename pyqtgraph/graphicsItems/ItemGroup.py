5bc27a3d56fb264a11305b27322388214cef00bd
Ashwola <schouderconstant@outlook.com>
2026-09-15 19:25:24 +0200

    changing ItemGroup to GroupItem

--- pyqtgraph/graphicsItems/ItemGroup.py
+++ pyqtgraph/graphicsItems/ItemGroup.py
@@ -1,23 +1,10 @@
-__all__ = ['ItemGroup']
+"""
+Deprecated: this module has been renamed to
+:mod:`pyqtgraph.graphicsItems.GroupItem`, and ``ItemGroup`` itself has been
+renamed to ``GroupItem``. Both names are kept here as aliases for backward
+compatibility and will be removed in a future version of pyqtgraph.
+"""
+from .._deprecated_names import RENAMED_SYMBOLS
+from .._deprecation import redirect_module
 
-from ..Qt import QtCore, QtWidgets
-from .GraphicsObject import GraphicsObject
-
-
-class ItemGroup(GraphicsObject):
-    """
-    Replacement for QGraphicsItemGroup
-    """
-    
-    def __init__(self, *args):
-        GraphicsObject.__init__(self, *args)
-        self.setFlag(QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemHasNoContents)
-    
-    def boundingRect(self):
-        return QtCore.QRectF()
-        
-    def paint(self, *args):
-        pass
-    
-    def addItem(self, item):
-        item.setParentItem(self)
+redirect_module(globals(), __name__, RENAMED_SYMBOLS)
