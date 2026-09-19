d3f112f2ec4fea83ed5bb39c114902c44581bc75
KIU Shueng Chuan <nixchuan@gmail.com>
2026-09-19 10:43:40 +0800

    GLSurfacePlot example: no need to precompute all frames
    tie animation rate to screen refresh rate

--- pyqtgraph/examples/GLSurfacePlot.py
+++ pyqtgraph/examples/GLSurfacePlot.py
@@ -2,13 +2,13 @@
 This example demonstrates the use of GLSurfacePlotItem.
 """
 import sys
+import time
 
 import numpy as np
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui
 import pyqtgraph.opengl as gl
-from pyqtgraph.Qt import QtCore
 
 if 'darwin' in sys.platform:
     fmt = QtGui.QSurfaceFormat()
@@ -70,33 +70,26 @@ w.addItem(p3)
 ## compute surface vertex data
 cols = 90
 rows = 100
-x = np.linspace(-8, 8, cols+1).reshape(cols+1,1)
-y = np.linspace(-8, 8, rows+1).reshape(1,rows+1)
+x = np.linspace(-8, 8, cols+1, dtype=np.float32).reshape((-1,1))
+y = np.linspace(-8, 8, rows+1, dtype=np.float32).reshape((1,-1))
 d = (x**2 + y**2) * 0.1
 d2 = d ** 0.5 + 0.1
-
-## precompute height values for all frames
-phi = np.arange(0, np.pi*2, np.pi/20.)
-z = np.sin(d[np.newaxis,...] + phi.reshape(phi.shape[0], 1, 1)) / d2[np.newaxis,...]
-
 
 ## create a surface plot, tell it to use the 'heightColor' shader
 ## since this does not require normal vectors to render (thus we 
 ## can set computeNormals=False to save time when the mesh updates)
 p4 = gl.GLSurfacePlotItem(x=x[:,0], y = y[0,:], shader='heightColor', computeNormals=False, smooth=False)
-p4.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
+p4.shader()['colorMap'] = [0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2]
 p4.translate(10, 10, 0)
 w.addItem(p4)
 
-index = 0
+time_start = time.perf_counter()
 def update():
-    global p4, z, index
-    index -= 1
-    p4.setData(z=z[index%z.shape[0]])
-    
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(30)
+    elapsed = time.perf_counter() - time_start
+    phi = 2*np.pi * (elapsed % 1.0)
+    z = np.sin(d + phi) / d2
+    p4.setData(z=z)
+w.frameSwapped.connect(update)
 
 if __name__ == '__main__':
     pg.exec()
